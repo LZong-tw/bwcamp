a0d8ba6a27c6bb70337f2dea4e80129dece2c202
LZong <lzong.tw@gmail.com>
2020-08-05 21:34:32 +0800

    Migration modified.

--- database/migrations/2020_07_30_142521_ycamp.php
+++ database/migrations/2020_07_30_142521_ycamp.php
@@ -17,8 +17,34 @@ class Ycamp extends Migration
         // https://docs.google.com/spreadsheets/d/1UXCVFgP8OXzr2fD_aiCnSbRW_zoQ_0Vu8MakmMOYuYc/
         Schema::create('ycamp', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('applicant_id')->constrained('applicants');
-            
+            $table->foreignId('applicant_id')->constrained('applicants');           
+            $table->string('school', 40)->nullable();
+            $table->string('school_location', 10)->nullable();
+            $table->string('region', 8)->nullable();
+            $table->string('day_night', 8)->nullable();
+            $table->string('system', 8)->nullable();
+            $table->string('department', 40)->nullable();
+            $table->string('grade', 10)->nullable();   
+            $table->string('way', 10)->nullable();   
+            $table->boolean('is_blisswisdom');   
+            $table->string('blisswisdom_type', 20)->nullable();   
+            // 
+            // 
+            // 
+            // father_name
+            // father_lamrim
+            // father_phone
+            // mother_name
+            // mother_lamrim
+            // mother_phone
+
+            // is_inperson
+            // agent_name
+            // agent_phone
+
+            // habbit
+            // club
+            // goal
             $table->timestamps();
         });
     }
