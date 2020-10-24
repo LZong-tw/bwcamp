db2df21927c49ff2bf4cae6dca480bf3c88dfd6b
LZong <lzong.tw@gmail.com>
2020-10-24 17:26:23 +0800

    Group list layout modified.

--- resources/views/backend/registration/groupList.blade.php
+++ resources/views/backend/registration/groupList.blade.php
@@ -3,17 +3,33 @@
     <h2>組別名單</h2>
     @foreach ($batches as $batch)
         梯次：{{ $batch->name }}
-        @foreach ($batch->regions as $region)
-            <table class="table table-bordered">
-                <thead><tr><th colspan="2">{{ $region->region }}</th></tr></thead>
-                @foreach ($region->groups as $group)
-                    <tr>
-                        <td>{{ $group->group }}</td>
-                        <td>{{ $group->count }}</td>
-                    </tr>
+        <table>
+            <tr>
+                @foreach ($batch->regions as $region)
+                <td style="vertical-align: top;">
+                    <table class="table table-bordered">
+                        <thead><tr class="bg-primary text-white"><th colspan="2">{{ $region->region }}</th></tr></thead>
+                        @php
+                            $count = 0;    
+                        @endphp
+                        @foreach ($region->groups as $group)
+                            <tr>
+                                <td>{{ $group->group }}</td>
+                                <td>{{ $group->count }}</td>
+                                @php
+                                    $count = $count + $group->count;
+                                @endphp
+                            </tr>
+                        @endforeach
+                        <tr class="bg-success text-white">
+                            <td>合計</td>
+                            <td>{{ $count }}</td>
+                        </tr>
+                    </table>
+                </td>
                 @endforeach
-            </table>
-        @endforeach
+            </tr>
+        </table>
         <hr>
     @endforeach
 @endsection
