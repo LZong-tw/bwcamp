7a4d89ee4d8ca5c6b3a54d1dbe5ff3472e2e67f9
LZong <lzong.tw@gmail.com>
2020-10-24 16:50:38 +0800

    Group list completed.

--- resources/views/backend/registration/groupList.blade.php
+++ resources/views/backend/registration/groupList.blade.php
@@ -2,6 +2,18 @@
 @section('content')
     <h2>組別名單</h2>
     @foreach ($batches as $batch)
-        {{ $batch->name }}
+        梯次：{{ $batch->name }}
+        @foreach ($batch->regions as $region)
+            <table class="table table-bordered">
+                <thead><tr><th colspan="2">{{ $region->region }}</th></tr></thead>
+                @foreach ($region->groups as $group)
+                    <tr>
+                        <td>{{ $group->group }}</td>
+                        <td>{{ $group->count }}</td>
+                    </tr>
+                @endforeach
+            </table>
+        @endforeach
+        <hr>
     @endforeach
 @endsection
