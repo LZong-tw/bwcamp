08d62a4753c48733afa2524388e0f3ee0270085f
LZong <lzong.tw@gmail.com>
2020-11-05 16:46:35 +0800

    Message modified.

--- resources/views/tcamp/modifyingSuccessful.blade.php
+++ resources/views/tcamp/modifyingSuccessful.blade.php
@@ -12,7 +12,7 @@
                 您成功修改報名 {{ $camp_data->fullName }}（簡稱本營隊）的個人資料。<br>
                 請記下您的<span class="text-danger font-weight-bold">《 報名序號：{{ $applicant->id }} 》</span>作為日後查詢使用。<br>
                 錄取結果將在 <u>{{ $camp_data->admission_announcing_date }} ({{ $admission_announcing_date_Weekday }})</u> 起於網上公佈，請自行上網查詢，<br>
-                並於 <u>{{ $camp_data->admission_confirming_end }} ({{ $admission_confirming_end_Weekday }})</u> 前上網回覆確認參加，倘未回覆，視同放棄。
+                錄取者收到「錄取繳費通知單」(或可於網站錄取頁面下載)後，請於 <span class="text-primary">12-08(星期二) ～ 12-14(星期一)</span> 期間內完成繳費，繳費後視同確定參加，倘未繳費，視同放棄。
             </p>
             <form action="{{ route("queryview", $applicant->batch_id) }}" method="post" class="d-inline">
                 @csrf
