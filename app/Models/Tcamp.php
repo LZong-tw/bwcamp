1efedd61090ccb703239769e694db5bdbce3879b
LZong <lzong.tw@gmail.com>
2020-11-05 16:59:49 +0800

    Bug fixed.

--- app/Models/Tcamp.php
+++ app/Models/Tcamp.php
@@ -10,7 +10,7 @@ class Tcamp extends Model
     protected $table = 'tcamp';
     
     protected $fillable = [
-        'applicant_id', 'is_educating', 'has_license', 'years_teached', 'education', 'school_or_course', 'subject_teaches', 'title', 'unit', 'unit_county', 'unit_district', 'is_blisswisdom', 'blisswisdom_type', 'blisswisdom_type_supplement'
+        'applicant_id', 'is_educating', 'has_license', 'years_teached', 'education', 'school_or_course', 'subject_teaches', 'title', 'unit', 'unit_county', 'unit_district', 'is_blisswisdom', 'blisswisdom_type', 'blisswisdom_type_complement'
     ];
 
     protected $guarded = [];
