bed72e1d163d186c056a60822ab52ff5b38d1ae6
LZong <lzong.tw@gmail.com>
2020-11-12 17:40:36 +0800

    Bug fixed.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
                     @endif
 
                     {{ __('You are logged in!') }} <br>
-                    @if(isset(auth()->user()->getPermission()->camp_id))
+                    @if(isset(auth()->user()->getPermission()))
                         <a href="{{ route("backendIndex") }}" class="btn btn-success">後台首頁</a>
                         <a href="" class="btn btn-primary">報到系統</a>
                     @endif
