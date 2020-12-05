57bcc8fa7170308bd7d17412cb32c889e4de0968
LZong <lzong.tw@gmail.com>
2020-12-05 10:21:45 +0800

    Text modified.

--- resources/views/tcamp/admittedMail.blade.php
+++ resources/views/tcamp/admittedMail.blade.php
@@ -20,7 +20,9 @@
     <li>繳費地點：可至超商、上海銀行繳費，或使用ATM轉帳、臨櫃匯款。</li>
     <li>若完成繳費，請於至少一個工作天後，上網查詢是否已繳費完畢。<br>
         （{{ url('camp/' . $applicant->batch_id . '/queryadmit') }}）</li>
-    <li>若繳費後，因故無法參加研習需退費者，請參照報名網申請退費注意事項（https://bwfoce.wixsite.com/bwtcamp/faq），並填寫退費申請單。</li>
+    <li>發票於營隊第一天提供，<strong>若需開立統一編號，請於 1/22 前填寫<a href="https://docs.google.com/forms/d/e/1FAIpQLSeVcqd01trNPKMSvc-RH8Zhac5Gexn-fBaAfAWMCn323PVgFw/viewform">申請單</a></strong>。</li>
+    <li>若繳費後，因故無法參加研習需退費者，請參照報名網申請退費注意事項（https://bwfoce.wixsite.com/bwtcamp/faq），並填寫<strong>退費申請單</strong>。</li>
+    <li><a style="color: red;">本會密切注意新冠疫情發展，若因故必須取消營隊或改變舉辦方式，將公布於教師營網頁。</a></li>
     <li>各區諮詢窗口<strong>（請於周一至周五 10:00~17:30 來電）</strong>：
         <table width="100%" style="table-layout:fixed; border: 0;">
             <tr>
