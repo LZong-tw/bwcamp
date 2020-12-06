53320fcf328eeffbe75137968046bfb359cda953
LZong <lzong.tw@gmail.com>
2020-12-06 23:26:03 +0800

    Strange bug patched.

--- resources/views/backend/registration/paymentFormPDF.blade.php
+++ resources/views/backend/registration/paymentFormPDF.blade.php
@@ -113,7 +113,7 @@
     <li>繳費地點：可至超商、上海銀行繳費，或使用ATM轉帳、臨櫃匯款。</li>
     <li>若完成繳費，請於至少一個工作天後，上網查詢是否已繳費完畢。<br>
         （<a href="http://bwcamp.bwfoce.org/camp/2/queryadmit" target="_blank" rel="noopener noreferrer">http://bwcamp.bwfoce.org/camp/2/queryadmit</a> ）</li>
-        {{-- <a href="{{ url('camp/' . $applicant->batch_id . '/queryadmit') }}">（{{ url('camp/' . $applicant->batch_id . '/queryadmit') }}）</a> --}}
+        {{-- （<a href="{{ url('camp/' . $applicant->batch_id . '/queryadmit') }}" target="blank">{{ url('camp/' . $applicant->batch_id . '/queryadmit') }}</a>） --}}
     <li>發票於營隊第一天提供，<strong>若需開立統一編號，請於 1/22 前填寫<a href="https://docs.google.com/forms/d/e/1FAIpQLSeVcqd01trNPKMSvc-RH8Zhac5Gexn-fBaAfAWMCn323PVgFw/viewform">申請單</a></strong>。</li>
     <li>若繳費後，因故無法參加研習需退費者，請參照<a href="https://bwfoce.wixsite.com/bwtcamp/faq">報名網站申請退費注意事項</a>，並填寫<strong>退費申請單</strong>。</li>
     <li><a style="color: red;">本會密切注意新冠疫情發展，若因故必須取消營隊或改變舉辦方式，將公布於教師營網頁。</a></li>
