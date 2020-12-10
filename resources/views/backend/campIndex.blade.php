38e680f30260853ae0bd25441591a0f80f3a911e
LZong <lzong.tw@gmail.com>
2020-12-10 17:48:32 +0800

    Information added.

--- resources/views/backend/campIndex.blade.php
+++ resources/views/backend/campIndex.blade.php
@@ -6,6 +6,7 @@
         <li><p>報名期間：{{ $campFullData->registration_start }} ~ {{ $campFullData->registration_end }}</p></li>
         <li><p>錄取公佈日：{{ $campFullData->admission_announcing_date }}</p></li>
         <li><p>確認參加期限：{{ $campFullData->admission_confirming_end }}</p></li>
+        <li><p>後台最終報名期限{{ $campFullData->final_registration_end }}</p></li>
         <li><p>繳費金額：{{ $campFullData->fee }}</p></li>
         <li><p>繳費期限(民國年 yymmdd)：{{ $campFullData->payment_deadline }}</p></li>
     </ul>
