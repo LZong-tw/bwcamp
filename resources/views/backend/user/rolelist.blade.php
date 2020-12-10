d1c7f06824f444498721a4008bd2a38606c4702a
LZong <lzong.tw@gmail.com>
2020-12-11 01:50:54 +0800

    Permission architect refactored.

--- resources/views/backend/user/rolelist.blade.php
+++ resources/views/backend/user/rolelist.blade.php
@@ -6,6 +6,7 @@
             <th>ID</th>
             <th>角色名稱</th>
             <th>等級</th>
+            <th>可存取的營隊 ID</th>
             <th>修改資料</th>
         </tr>
         @foreach($roles as $role)
@@ -13,6 +14,7 @@
                 <td>{{ $role->id }}</td>
                 <td>{{ $role->name }}</td>
                 <td>{{ $role->level }}</td>
+                <td><a href="{{ route("campIndex", $role->camp_id ?? "") }}">{{ $role->camp_id }}</a></td>
                 <td><a href="" class="btn btn-success">修改</a></td>
             </tr>
         @endforeach
