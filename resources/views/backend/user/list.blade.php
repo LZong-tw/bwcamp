64be420b1e81f919bb6453b0d48177841373f9da
LZong <lzong.tw@gmail.com>
2020-12-31 16:24:23 +0800

    Link modified.

--- resources/views/backend/user/list.blade.php
+++ resources/views/backend/user/list.blade.php
@@ -25,13 +25,13 @@
                 <td>{{ $user->email }}</td>
                 <td>@foreach ($user->role_relations as $role_relation)
                     @if($loop->last)
-                        {{ $role_relation->role_data->level }} - {{ $role_relation->role_data->name }} - <a href="{{ route("campIndex", $role_relation->role_data->camp_id ?? "") }}" class="card-link">{{ $role_relation->role_data->camp->abbreviation ?? "" }}</a>
+                        {{ $role_relation->role_data->level }} - {{ $role_relation->role_data->name }} - <a href="{{ route("campIndex", $role_relation->role_data->camp_id ?? "") }}" class="card-link" target="_blank">{{ $role_relation->role_data->camp->abbreviation ?? "" }}</a>
                     @else
-                        {{ $role_relation->role_data->level }} - {{ $role_relation->role_data->name }} - <a href="{{ route("campIndex", $role_relation->role_data->camp_id ?? "") }}" class="card-link">{{ $role_relation->role_data->camp->abbreviation ?? "" }}</a><br>
+                        {{ $role_relation->role_data->level }} - {{ $role_relation->role_data->name }} - <a href="{{ route("campIndex", $role_relation->role_data->camp_id ?? "") }}" class="card-link" target="_blank">{{ $role_relation->role_data->camp->abbreviation ?? "" }}</a><br>
                     @endif
                 @endforeach</td>
-                <td><a href="" class="btn btn-success">修改</a></td>
-                <td><a href="{{ route("editRole", [$user->id]) }}" class="btn btn-primary">設定</a></td>
+                <td><a href="" class="btn btn-success" target="_blank">修改</a></td>
+                <td><a href="{{ route("editRole", [$user->id]) }}" class="btn btn-primary" target="_blank">設定</a></td>
             </tr>
         @endforeach
     </table>
