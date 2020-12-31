a3046fdd21cb0faa654bb97041a424131ec1efb7
LZong <lzong.tw@gmail.com>
2021-01-01 01:41:38 +0800

    Role edit mock-up.

--- resources/views/backend/user/rolelist.blade.php
+++ resources/views/backend/user/rolelist.blade.php
@@ -8,7 +8,8 @@
             color: #33B2FF!important;
         }
     </style>
-    <h2>權限列表</h2>
+    <h2 class="d-inline-block">權限列表</h2>
+    <a href="" class="btn btn-success d-inline-block" style="margin-bottom: 10px">新增角色</a>
     <table class="table table-bordered">
         <tr>
             <th>ID</th>
@@ -16,6 +17,7 @@
             <th>等級</th>
             <th>可存取的營隊</th>
             <th>修改資料</th>
+            <th>刪除角色</th>
         </tr>
         @foreach($roles as $role)
             <tr>
@@ -23,7 +25,8 @@
                 <td>{{ $role->name }}</td>
                 <td>{{ $role->level }}</td>
                 <td><a href="{{ route("campIndex", $role->camp_id ?? "") }}" class="card-link" target="_blank">{{ $role->camp->abbreviation ?? "" }}</a></td>
-                <td><a href="" class="btn btn-success">修改</a></td>
+                <td><a href="" class="btn btn-primary">修改</a></td>
+                <td><a href="" class="btn btn-danger">刪除</a></td>
             </tr>
         @endforeach
     </table>
