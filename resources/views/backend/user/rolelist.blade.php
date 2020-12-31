14774474332913ae98a7293b2fbb48d70b527af2
LZong <lzong.tw@gmail.com>
2020-12-31 16:28:56 +0800

    Layout modified.

--- resources/views/backend/user/rolelist.blade.php
+++ resources/views/backend/user/rolelist.blade.php
@@ -1,12 +1,20 @@
 @extends('backend.master')
 @section('content')
+    <style>
+        .card-link{
+            color: #3F86FB!important;
+        }
+        .card-link:hover{
+            color: #33B2FF!important;
+        }
+    </style>
     <h2>權限列表</h2>
     <table class="table table-bordered">
         <tr>
             <th>ID</th>
             <th>角色名稱</th>
             <th>等級</th>
-            <th>可存取的營隊 ID</th>
+            <th>可存取的營隊</th>
             <th>修改資料</th>
         </tr>
         @foreach($roles as $role)
@@ -14,7 +22,7 @@
                 <td>{{ $role->id }}</td>
                 <td>{{ $role->name }}</td>
                 <td>{{ $role->level }}</td>
-                <td><a href="{{ route("campIndex", $role->camp_id ?? "") }}">{{ $role->camp_id }}</a></td>
+                <td><a href="{{ route("campIndex", $role->camp_id ?? "") }}" class="card-link" target="_blank">{{ $role->camp->abbreviation ?? "" }}</a></td>
                 <td><a href="" class="btn btn-success">修改</a></td>
             </tr>
         @endforeach
