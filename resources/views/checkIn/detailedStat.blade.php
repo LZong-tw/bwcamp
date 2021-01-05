8ac9a15bab89b13bdf9c53a32e794ecc301dcefa
LZong <lzong.tw@gmail.com>
2021-01-05 21:07:57 +0800

    Bug fixed, layout modified.

--- resources/views/checkIn/detailedStat.blade.php
+++ resources/views/checkIn/detailedStat.blade.php
@@ -3,13 +3,16 @@
 <div class="container">
     <h2 class="mt-4 text-center">福智營隊報到系統 - 今日各梯次即時報到資料</h2>
     <h5 class="text-center">當前報到營隊：{{ $camp->fullName }}<br>報到日期：{{ \Carbon\Carbon::today()->format('Y-m-d') }}</h5>       
-    <h6 class="text-center">每 20 秒自動更新資料 &nbsp;<a href="#" onclick="this.innerText = '更新中...'; location.reload();">手動更新資料</a></h6>
+    <h6 class="text-center">每 12 秒自動更新資料 &nbsp;<a href="#" id="tip" onclick="this.innerText = '更新中...'; location.reload();">手動更新資料</a></h6>
     <div id="data">
         @foreach ($batchArray as $batch)
             @if(!$batch)
                 @continue
             @endif
-            <h3>梯次：{{ $batch['name'] }}</h3>
+            <div class="mb-1">
+                <h3 class="d-inline">梯次：{{ $batch['name'] }}</h3>
+                &nbsp;<a href="" class="d-inline">組別名單</a>
+            </div>
             <h4>累積報到人數 / 未報到人數：{{ $batch['checkedInApplicants'] }} / {{ $batch['allApplicants'] - $batch['checkedInApplicants'] }}</h4>
             @if(!$loop->last)
                 <hr>
@@ -18,19 +21,9 @@
     </div>
 </div>  
 <script>
-    httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function(){
-        // Process the server response here.
-        if (httpRequest.status === 200) {
-            // Perfect!
-            console.log(httpRequest.responseText);
-        } else {
-            // There was a problem with the request.
-            // For example, the response may have a 404 (Not Found)
-            // or 500 (Internal Server Error) response code.
-        }
-    };
-    httpRequest.open('GET', '{{ url("") }}/checkin/realtimeStat', true);
-    httpRequest.send();
+    window.setTimeout(function () {
+        document.getElementById("tip").innerHTML = "更新中...";
+        window.location.reload();
+    }, 12000);
 </script>
 @endsection
