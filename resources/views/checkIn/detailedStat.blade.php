e1ee61e193aedff34362d619877187f0288c0c24
LZong <lzong.tw@gmail.com>
2021-01-24 16:33:56 +0800

    Division by zero avoided.

--- resources/views/checkIn/detailedStat.blade.php
+++ resources/views/checkIn/detailedStat.blade.php
@@ -5,7 +5,7 @@
     <h5 class="text-center">當前報到營隊：{{ $camp->fullName }}<br>報到日期：{{ \Carbon\Carbon::today()->format('Y-m-d') }}</h5>       
     <h6 class="text-center">每 15 秒自動更新資料 &nbsp;<a href="#" id="tip" onclick="this.innerText = '更新中...'; location.reload();">手動更新資料</a></h6>
     <h3>全梯次</h3>
-    <h4>累積報到人數 / 未報到人數：{{ $checkedInCount }} / {{ $applicantsCount - $checkedInCount }}&nbsp;&nbsp;&nbsp;&nbsp;報到率：{{ round($checkedInCount / $applicantsCount * 100, 2) }}%</h4>
+    <h4>累積報到人數 / 未報到人數：{{ $checkedInCount }} / {{ $applicantsCount - $checkedInCount }}&nbsp;&nbsp;&nbsp;&nbsp;報到率：{{ round($checkedInCount / max($applicantsCount, 1) * 100, 2) }}%</h4>
     <hr>
     <div id="data">
         @foreach ($batchArray as $batch)
@@ -15,7 +15,7 @@
             <div class="mb-1">
                 <h3 class="d-inline">梯次：{{ $batch['name'] }}</h3>
             </div>
-            <h4>累積報到人數 / 未報到人數：{{ $batch['checkedInApplicants'] }} / {{ $batch['allApplicants'] - $batch['checkedInApplicants'] }}&nbsp;&nbsp;&nbsp;&nbsp;報到率：{{ round($batch['checkedInApplicants'] / $batch['allApplicants'] * 100, 2) }}%</h4>
+            <h4>累積報到人數 / 未報到人數：{{ $batch['checkedInApplicants'] }} / {{ $batch['allApplicants'] - $batch['checkedInApplicants'] }}&nbsp;&nbsp;&nbsp;&nbsp;報到率：{{ round($batch['checkedInApplicants'] / max($batch['allApplicants'], 1) * 100, 2) }}%</h4>
             @if(!$loop->last)
                 <hr>
             @endif
