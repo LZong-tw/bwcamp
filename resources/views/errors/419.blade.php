feb473c6f7951718bcdadf9d9d1fc6415b8020e9
LZong <lzong.tw@gmail.com>
2021-01-30 13:24:01 +0800

    Error message modified, readme modified.

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -53,8 +53,8 @@
             <div class="code">
                 419            </div>
 
-            <div class="message" style="padding: 10px;">
-                頁面過期，請回到上一頁重新整理後，再重新操作</div>
+            <div class="message" style="padding: 10px; width: 50%">
+                頁面過期，請回到上一頁重新整理後，再重新操作；若為 <u style="color: red"><strong>iPhone, iPad</strong></u> 的 <u style="color: red"><strong>Safari</strong></u> 使用者，請於手機設定中檢查 Safari 的「阻擋所有 cookie」是否已關閉，再回到上一頁重新整理後重試</div>
         </div>
     </body>
 </html>
