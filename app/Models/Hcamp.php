b05e7a5d8e1e400667c89c82d43e4f244ca7932e
LZong <lzong.tw@gmail.com>
2021-02-26 16:49:26 +0800

    Hcamp model added.

---
+++ app/Models/Hcamp.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tcamp extends Model
+{
+    //
+    protected $table = 'hcamp';
+    
+    protected $fillable = [
+        'applicant_id', 'education', 'special_condition', 'traffic', 'branch_or_classroom_belongs_to', 'class_type', 'parent_lamrim_class', 'is_recommended_by_reading_class'
+    ];
+
+    protected $guarded = [];
+}
