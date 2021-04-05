80938d5130e6289c335af8969fc049c31ccbb7a1
LZong <lzong.tw@gmail.com>
2021-04-05 12:10:24 +0800

    Message modified, typo fixed.

--- resources/views/camps/hcamp/applicantMail.blade.php
+++ resources/views/camps/hcamp/applicantMail.blade.php
@@ -6,7 +6,7 @@
 另請您記下您的報名序號：{{ $applicant->id }} 作為日後查詢使用。<br>
 如需修改資料、下載繳費單或退費申請等，請連結以下網址：<a href="{{ url(route("query", $applicant->batch_id)) }}">{{ route("query", $applicant->batch_id) }}</a><br>
 <br>
-<strong>★提醒您：4/20前報名並繳費始享有早鳥優惠價格；4/21起即恢復原價5500元，若未於5/4前未繳費，則尚失錄取資格。</strong><br>
+<strong>★提醒您：4/20前報名並繳費始享有早鳥優惠價格；4/21起即恢復原價5500元，若未於5/4前未繳費，則喪失錄取資格。</strong><br>
 <br>
 營隊問題諮詢方式如下：<br>
 一、可掃描 QR-Code 或點選連結 https://line.me/R/ti/g/HbQvGwJyng <br>
