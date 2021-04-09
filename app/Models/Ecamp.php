b3ccafdc64567f557b54b015aca1071c4694e348
LZong <lzong.tw@gmail.com>
2021-04-10 00:06:30 +0800

    Modified fillable of ecamp model.

--- app/Models/Ecamp.php
+++ app/Models/Ecamp.php
@@ -10,7 +10,7 @@ class Ecamp extends Model
     protected $table = 'ecamp';
     
     protected $fillable = [
-        'applicant_id', 'belief', 'education', 'unit', 'unit_location', 'title', 'level', 'job_property', 'experience', 'employees', 'direct_managed_employees', 'industry'
+        'applicant_id', 'belief', 'education', 'unit', 'unit_location', 'title', 'level', 'job_property', 'experience', 'employees', 'direct_managed_employees', 'industry', 'after_camp_available_day', 'favored_event'
     ];
 
     protected $guarded = [];
