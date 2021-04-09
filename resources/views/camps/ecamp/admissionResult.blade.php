25d40aab3d141a1cba3b9b2577a8d733e2600aed
Minuet <menuett@gmail.com>
2021-04-10 00:09:27 +0800

    fix camps.ecamp.layout

--- resources/views/camps/ecamp/admissionResult.blade.php
+++ resources/views/camps/ecamp/admissionResult.blade.php
@@ -1,4 +1,4 @@
-@extends('camps.tcamp.layout')
+@extends('camps.ecamp.layout')
 @section('content')
     <style>
         .indent{
--- resources/views/camps/ecamp/success.blade.php
+++ resources/views/camps/ecamp/success.blade.php
@@ -1,4 +1,4 @@
-@extends('camps.tcamp.layout')
+@extends('camps.ecamp.layout')
 @section('content')
     <div class='page-header form-group'>
         <h4>{{ $camp_data->fullName }}</h4>
