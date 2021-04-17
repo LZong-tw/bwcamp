7707c5c287dd148ba527ac0666fffaef1672dd3d
Minuet <menuett@gmail.com>
2021-04-17 11:33:38 +0800

    ycamp confirm mail

--- resources/views/camps/ycamp/applicantMail.blade.php
+++ resources/views/camps/ycamp/applicantMail.blade.php
@@ -2,9 +2,9 @@
 <br>
     感謝您報名{{ $applicant->batch->camp->fullName }}，報名手續已完成，<br>
     請記下您的報名序號： <strong>{{ $applicant->id }}</strong> 作為日後查詢使用。<br>
-    錄取名單將於 {{ $campData->admission_announcing_date }} 於網站公佈：https://bwyouth.org/camp <br>
+    錄取名單將於 {{ $campData->admission_announcing_date }} 於網站公佈：http://bwyouth.org/camp <br>
     <br>
-    洽詢電話：(02)7751-6799分機520037 (週一 ~ 週五 上午10時 ~ 下午5時) <br>
+    洽詢電話：(週一 ~ 週五 上午10時 ~ 下午5時) <br>
     台北：02-2545-3788 #541<br>
     桃園：03-275-6133 #1324<br>
     新竹：03-571-0968<br>
