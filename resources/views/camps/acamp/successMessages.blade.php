5935a6912994afcbd07dcae6ab7defe4710a44a4
Minuet <menuett@gmail.com>
2021-04-17 23:49:12 +0800

    modify acamp confirm message content

--- resources/views/camps/acamp/successMessages.blade.php
+++ resources/views/camps/acamp/successMessages.blade.php
@@ -1,4 +1,4 @@
 
-請記下您的<span class="text-danger font-weight-bold">《 報名序號：{{ $applicant->id }} 》</span>作為日後查詢使用。
-本營隊將於 <u>{{ $camp_data->admission_announcing_date }} ({{ $admission_announcing_date_Weekday }})</u> 於官組公布錄取名單，並提供錄取查詢。
+請記下您的<span class="text-danger font-weight-bold">《 報名序號：{{ $applicant->id }} 》</span>作為日後查詢使用。<br><br>
+本營隊將於 <u>{{ $camp_data->admission_announcing_date }} ({{ $admission_announcing_date_Weekday }})</u> 於官網公布錄取名單，並提供錄取查詢。
 <br>
