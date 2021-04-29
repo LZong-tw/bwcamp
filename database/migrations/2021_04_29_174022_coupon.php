cda8155513a65738e7ad99e4720e64bc9081f4ff
LZong <lzong.tw@gmail.com>
2021-04-29 18:19:51 +0800

    Migration added.

---
+++ database/migrations/2021_04_29_174022_coupon.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Coupon extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupon', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('applicant_id')->constrained('applicants'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('tcamp');
+    }
+}
