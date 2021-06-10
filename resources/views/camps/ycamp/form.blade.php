b45691ba9bb9f2eb8ae329cf3494edaff78ef520
Minuet <menuett@gmail.com>
2021-06-10 19:22:01 +0800

    ycamp change to online

--- resources/views/camps/ycamp/form.blade.php
+++ resources/views/camps/ycamp/form.blade.php
@@ -37,6 +37,8 @@ header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
     <div class='row form-group'>
         <label for='inputBatch' class='col-md-2 control-label text-md-right'>營隊梯次</label>
         <div class='col-md-10'>
+            <h3>線上</h3>
+            {{--
             @if(isset($applicant_data))
                 <h3>{{ $applicant_raw_data->batch->name . '梯' }} {{ $applicant_raw_data->batch->batch_start }} ~ {{ $applicant_raw_data->batch->batch_end }} </h3>
                 <input type='hidden' name='applicant_id' value='{{ $applicant_id }}'>
@@ -45,6 +47,7 @@ header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
                 <a href='https://bwyouth.wixsite.com/camp2021'><span class='text-danger'>點此報名別梯</span></a>
                 </h3>                
             @endif
+            --}}
         </div>
     </div>
     @if(isset($isModify))
