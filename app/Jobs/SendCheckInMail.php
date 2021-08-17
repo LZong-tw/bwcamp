9e5920d836c9b4c016b359ac9944a02517396587
LZong <lzong.tw@gmail.com>
2021-08-17 23:54:39 +0800

    Acamp check-in mail.

--- app/Jobs/SendCheckInMail.php
+++ app/Jobs/SendCheckInMail.php
@@ -50,6 +50,11 @@ class SendCheckInMail implements ShouldQueue
         $attachment =  $pdf->stream();
         // 動態載入電子郵件設定
         $this->setEmail($this->applicant->batch->camp->table);
-        \Mail::to($this->applicant->email)->send(new \App\Mail\CheckInMail($this->applicant, $attachment));
+        if($this->applicant->batch->camp->table == "acamp"){
+            \Mail::to($this->applicant->email)->send(new \App\Mail\CheckInMail($this->applicant));    
+        }
+        else{
+            \Mail::to($this->applicant->email)->send(new \App\Mail\CheckInMail($this->applicant, $attachment));
+        }
     }
 }
