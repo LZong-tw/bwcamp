c979bf2b1147c187c242874b1814e44801be8c71
LZong <lzong.tw@gmail.com>
2021-12-09 14:56:49 +0800

    Tcamp admission messages modified.

--- resources/views/camps/tcamp/admittedMail.blade.php
+++ resources/views/camps/tcamp/admittedMail.blade.php
@@ -1,7 +1,9 @@
 <style>
-    u{
-        color: red;
-    }
+    @if($applicant->batch->camp->variant == "utcamp") 
+        u{
+            color: red;
+        }
+    @endif
 
     .center{
         text-align: center;
@@ -15,11 +17,13 @@
     <h2 class="center">2022第30屆教師生命成長營</h2> 
     <h2 class="center">大專教職員梯 錄取/確認參加 通知單</h2> 
 @else
-    <h2 class="center">{{ $applicant->batch->camp->fullName }} 錄取通知單</h2>
+    <h2 class="center">2022教師生命成長營錄取通知單</h2>
 @endif
 <table width="100%" style="table-layout:fixed; border: 0;">
     <tr>
-        {{-- <td>梯次：{{ $applicant->batch->name }}</td> --}}
+        @if(!$applicant->batch->camp->variant)
+            <td>場次：幼小中高場(1/26-27)</td>
+        @endif
         <td>姓名：{{ $applicant->name }}</td>
         <td>錄取編號：<u>{{ $applicant->group }}{{ $applicant->number }}</u></td>
         <td>組別：<u>{{ $applicant->group }}</u></td>
@@ -38,39 +42,27 @@
     </ol>
     若有任何問題，歡迎與各組關懷員聯絡，或來電02-7751-6799 #520023邱先生(2022第30屆教師生命成長營大專教職員梯秘書組)。<br>
 @else
-
+    恭喜您錄取「2022教師生命成長營」！竭誠歡迎您的到來，期待與您共享這場心靈饗宴，希望您能獲得豐盛的收穫。我們會在2022年1/10〜1/12寄送幸福魔法盒(教材包)，屆時請再注意收件。這次的營隊內容結合魔法學校的情境設計元素，以下先做簡單說明：<br>
+    <ul>
+        <li>
+            情境設計 <br>
+            2021年11月1日凌晨，因魔咒解除，霍福華智魔法學校重現世間，從水底深處浮出水面。頓時撼動天上人間，披風家族乘坐魔帚從四面八方凌空而來，一道道光軌劃亮夜空，迎來最明亮潔白的圓月出現天際，共同迎接萬年盛事～古堡矗立，魔法重現，不可思議即將展開~ 
+            <hr>
+            霍福華智魔法學校首辦2022年教師遊學課程--教師生命成長營，在經過一個多月的報名、審核的過程後，確認有900多位老師們身上流有魔法師的血液，具備初級魔法師的資格，有辦法進行魔法學習。魔法學校校長福智利多很重視這群新到來的初級魔法師，畢竟在麻瓜的世界裡，要找到這群人、延續魔法的血脈很不容易，因此，特別選定經過訓練後的中級魔法師們，以及特派披風家族守護員，來接引他們到學校學習。
+            <hr>
+            在入學前，初級魔法師們就會收到一封由貓頭鷹嘿美送到的入學通知書，說明入學前的準備工作、班級班號、加入線上平台、如何到達學校等。接著，會開始接到中級魔法師的電話通知、參加遊學前的兩次行前說明會，並收到幸福魔法盒、取得魔法特快車的月台車票，做好遊學前的準備工作。這段期間，也會在魔法學校和幸福心學堂online合作的線上平台上看到各種訊息、進行各種活動。
+            <hr>
+            入學當天，他們將拿著月台車票，通過九又五分之四月台，搭上魔法特快車、專屬他的班級的車廂，一起前往魔法學校。這次的遊學課程，除了兩天的營隊模式，介紹魔法學校的學習主軸和魔法體驗之外，還有長達一年的深入學習，讓魔法師們可以透過多角度的了解、討論、實作，熟練魔法的使用，堅固施展魔法解決問題、幫助別人的能力。
+        </li>
+        <li>
+            請加入幸福心學堂online臉書社團：<br>
+            <a href="https://www.facebook.com/groups/bwfoce.happiness.new" target="_blank" rel="noopener noreferrer">https://www.facebook.com/groups/bwfoce.happiness.new</a>
+        </li>
+        <li>
+            諮詢窗口（請於周一至周五 10:00~17:30 來電）：<br>
+            台北場　劉小姐 (02)2545-3788#529
+        </li>
+    </ul>
 @endif
-
-{{-- 恭喜您錄取「{{ $applicant->batch->camp->fullName }} 」！竭誠歡迎您的到來，期待與您共享這場心靈饗宴，希望您能獲得豐盛的收穫。以下幾點事情需要您的協助與配合：<br>
-<ul>
-    <li>活動費用：1200元，繳費聯請下載附件檔。</li>
-    <li>請於{{ \Carbon\Carbon::now()->year }}年{{ substr($campFullData->payment_deadline, 2, 2) }}月{{ substr($campFullData->payment_deadline, 4, 2) }}日前完成繳費，<a style="color: red;">逾時將<u>視同放棄錄取資格</u>！</a></li>
-    <li>繳費地點：可至超商、上海銀行繳費，或使用ATM轉帳、臨櫃匯款。</li>
-    <li>若完成繳費，請於至少一個工作天後，上網查詢是否已繳費完畢。<br>
-        （<a href="http://bwcamp.bwfoce.org/camp/2/queryadmit" target="_blank" rel="noopener noreferrer">http://bwcamp.bwfoce.org/camp/2/queryadmit</a> ）</li>
-    <li>發票於營隊第一天提供，<strong>若需開立統一編號，請於 1/22 前填寫<a href="https://docs.google.com/forms/d/e/1FAIpQLSeVcqd01trNPKMSvc-RH8Zhac5Gexn-fBaAfAWMCn323PVgFw/viewform" target="_blank">申請單</a></strong>。</li>
-    <li>若繳費後，因故無法參加研習需退費者，請參照<a href="https://bwfoce.wixsite.com/bwtcamp/faq" target="_blank">報名網站申請退費注意事項</a>，並填寫<strong>退費申請單</strong>。</li>
-    <li><a style="color: red;">本會密切注意新冠疫情發展，若因故必須取消營隊或改變舉辦方式，將公布於教師營網頁。</a></li>
-    <li>各區諮詢窗口<strong>（請於周一至周五 10:00~17:30 來電）</strong>：
-        <table width="100%" style="table-layout:fixed; border: 0;">
-            <tr>
-                <td>台北場　劉小姐 (02)2545-3788#529</td>
-                <td>雲林場　吳小姐0921-013450</td>
-            </tr>
-            <tr>
-                <td>桃園場　趙小姐  (03)275-6133#1312</td>
-                <td>嘉義場　吳小姐0928-780108</td>
-            </tr>
-            <tr>
-                <td>新竹場　張小姐 (03)532-5566#246</td>
-                <td>台南場　簡小姐0919-852066</td>
-            </tr>
-            <tr>
-                <td>台中場　蔣小姐  0933-199203</td>
-                <td>高雄場　胡小姐(07)9769341#417</td>
-            </tr>
-        </table>	
-	</li>
-</ul> --}}
-<a class="right">財團法人福智文教基金會　謹此</a><br> 
+<a class="right">財團法人福智文教基金會　@if($applicant->batch->camp->variant == "utcamp") 謹此 @else 敬啟 @endif</a><br> 
 <a class="right">{{ \Carbon\Carbon::now()->year }}  年　{{ \Carbon\Carbon::now()->month }}  月 　 {{ \Carbon\Carbon::now()->day }}  日</a>
