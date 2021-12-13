d7832c062fe5e903cd118936fe2cec8cb36dbea1
LZong <lzong.tw@gmail.com>
2021-12-13 21:56:23 +0800

    Typo fixed.

--- resources/views/camps/tcamp/admissionResult.blade.php
+++ resources/views/camps/tcamp/admissionResult.blade.php
@@ -205,7 +205,7 @@
                                     「教師生命成長營」自舉辦以來，每年都得到教育夥伴們的支持和肯定，思及社會上仍有這麼多人共同關心莘莘學子們的學習成長，令人深感振奮！每一位老師的報名都是鼓舞我們的一分力量，激勵基金會全體人員持續不懈，與大家共同攜手為教育盡心盡力。
                                     非常感謝您的報名，由於我們的視訊配備侷限，不克錄取，造成您的不便，敬請見諒包涵！ <br><br>
                                     福智文教基金會在全省各縣市的分支機構，平日都設有適合各年齡層的多元心靈提升課程，誠摯歡迎您的參與！<br><br>
-                                    關注「福智文教基金會」網站：<a href="bwfoce.org" target="_blank" rel="noopener noreferrer">bwfoce.org</a><br>
+                                    關注「福智文教基金會」網站：<a href="https://bwfoce.org" target="_blank" rel="noopener noreferrer">https://bwfoce.org</a><br>
                                     關注「哈特麥1D」(heart mind edu.)FB：<a href="https://www.facebook.com/heartmind1d/" target="_blank" rel="noopener noreferrer">https://www.facebook.com/heartmind1d/</a><br>
                                     <br>
                                     祝福　教安，健康平安！ 
