cde07a7e7c30f4182329a98bf0ffb6c557e20b09
Minuet <menuett@gmail.com>
2022-03-06 14:14:03 +0800

    Modify ceovcamp fields

--- app/Models/CEOVcamp.php
+++ app/Models/CEOVcamp.php
@@ -11,10 +11,10 @@ class Ceovcamp extends Model
     
     protected $fillable = [
         'applicant_id', 'group_priority1', 'group_priority2', 'group_priority3', 'group_priority_other',
-        'lrclass', 'transport', 'transport_other', 'expertise', 'language',
-        'language_other', 'unit', 'industry', 'industry_other', 'title',
-        'job_property', 'job_property_other', 'employees', 'direct_managed_employees', 'capital',
-        'org_type', 'org_type_other', 'years_operation'
+        'lrclass', 'cadre_experiences', 'volunteer_experiences', 'transport', 'transport_other', 'expertise', 'language',
+        'language_other', 'unit', 'industry', 'title',
+        'job_property', 'employees', 'direct_managed_employees', 'capital',
+        'org_type', 'years_operation'
     ];
 
     protected $guarded = [];
