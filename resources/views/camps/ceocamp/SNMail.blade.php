ecea2d13b66f498090bae8c9c03aaac60109e4ac
Minuet <menuett@gmail.com>
2022-03-10 21:14:48 +0800

    ceocamp: send email to applicant, introducer, and substitute.

--- resources/views/camps/ceocamp/SNMail.blade.php
+++ resources/views/camps/ceocamp/SNMail.blade.php
@@ -1,1 +1,1 @@
-您好，被推薦人 {{ $applicant->name }} 的報名序號為<span class="text-danger font-weight-bold">《 {{ $applicant->applicant_id }} 》</span>。
+您好，被推薦人 {{ $applicant->name }} 的報名序號為<span class="text-danger font-weight-bold">《 {{ $applicant->id }} 》</span>。
