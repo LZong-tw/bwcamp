af4907148180fb95a8ccd3dd08604fa93f0705bf
Minuet <menuett@gmail.com>
2022-03-13 20:04:36 +0800

    modify ceovcamp/ecamp mail contents

--- resources/views/camps/ecamp/successMessages.blade.php
+++ resources/views/camps/ecamp/successMessages.blade.php
@@ -1,4 +1,4 @@
 
 請記下您的<span class="text-danger font-weight-bold">《 報名序號：{{ $applicant->id }} 》</span>作為日後查詢使用。
-錄取結果將在 <u>{{ $camp_data->admission_announcing_date }} ({{ $admission_announcing_date_Weekday }})</u> 後陸續以E-mail通知。
+<u>{{ $camp_data->admission_announcing_date }} ({{ $admission_announcing_date_Weekday }})</u>以後將以E-mail通知您營隊後續訊息。
 <br>
