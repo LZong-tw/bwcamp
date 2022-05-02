9f53c6680f8dcff9d4033edf8b8117ba8804e885
Minuet <menuett@gmail.com>
2022-05-02 14:34:15 +0800

    ycamp fix form bugs

--- app/Models/Ycamp.php
+++ app/Models/Ycamp.php
@@ -10,7 +10,7 @@ class Ycamp extends Model
     protected $table = 'ycamp';
     
     protected $fillable = [
-        'applicant_id', 'school', 'school_location', 'day_night', 'system', 'department', 'grade', 'way', 'is_blisswisdom', 'blisswisdom_type', 'father_name', 'father_lamrim', 'father_phone', 'mother_name', 'mother_lamrim', 'mother_phone', 'is_inperson', 'agent_name', 'agent_phone', 'habbit', 'club', 'goal',
+        'applicant_id', 'school', 'school_location', 'day_night', 'system', 'department', 'grade', 'way', 'is_blisswisdom', 'blisswisdom_type', 'blisswisdom_type_other', 'father_name', 'father_lamrim', 'father_phone', 'mother_name', 'mother_lamrim', 'mother_phone', 'is_inperson', 'agent_name', 'agent_phone', 'habbit', 'club', 'goal',
     ];
 
     protected $guarded = [];
