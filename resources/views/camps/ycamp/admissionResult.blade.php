1cee0bb7f245fd47fbe9ec4ca0735202e4d8761c
Minuet <menuett@gmail.com>
2022-06-23 12:31:04 +0800

    ycamp: all online

--- resources/views/camps/ycamp/admissionResult.blade.php
+++ resources/views/camps/ycamp/admissionResult.blade.php
@@ -32,14 +32,15 @@
                 <p class="card-text indent">
                 錄取編號：{{ $applicant->group }}{{ $applicant->number }}<br>
                 營隊場次：{{ $applicant->batch->name }} <br>
-                營隊日期：{{ $applicant->batch->batch_start }} ~ {{ $applicant->batch->batch_end }} <br>
+                營隊日期：{{ $applicant->batch->batch_start }} ~ {{ $applicant->batch->batch_end }}（六、日），共2天<br>
                 </p>
                 <p class="card-text indent">期待與您共享這場心靈饗宴！請詳閱以下相關訊息，祝福您營隊收穫滿滿。</p>
 
                 <p class="card-text">
                     <h5>營隊相關訊息</h5>
-                        <div class="ml-0 mb-2">1.經錄取後，敬請全程參與本活動。全程參與者，發給研習證明文件。</div>
-                        <div class="ml-0 mb-2">2.有任何問題，請Email至<a href="mailto:youth@blisswisdom.org">youth@blisswisdom.org</a>，或於<a href="https://www.facebook.com/bwyouth" target="_blank" rel="noopener noreferrer">福智青年粉專</a>留言</div>
+                        <div class="ml-0 mb-2">1.因應疫情，營隊各梯次皆改為線上舉辦。</div>
+                        <div class="ml-0 mb-2">2.經錄取後，敬請全程參與本活動。全程參與者，發給研習證明文件。</div>
+                        <div class="ml-0 mb-2">3.有任何問題，請Email至<a href="mailto:youth@blisswisdom.org">youth@blisswisdom.org</a>，或於<a href="https://www.facebook.com/bwyouth" target="_blank" rel="noopener noreferrer">福智青年粉專</a>留言</div>
                     
 <!--
                     <h4>確認參加</h4>
