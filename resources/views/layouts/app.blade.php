8fbded3e5036730eba10ed60182c13340795ca85
LZong <lzong.tw@gmail.com>
2022-10-26 22:49:18 +0800

    Enhanced.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,10 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('/home') }}">
+                                        {{ __('Home') }}
+                                    </a>
+
                                     <a class="dropdown-item" href="{{ route('profile') }}">
                                         {{ __('Profile') }}
                                     </a>
