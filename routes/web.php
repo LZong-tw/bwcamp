9ba18e86dd75498c1ff2bebf8d9f51d9bb12f797
LZong <lzong.tw@gmail.com>
2022-10-26 22:41:17 +0800

    Bugs fixed.

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::post("password/email", "Auth\ForgotPasswordController@sendResetLinkEmail"
 Route::get("password/reset/{token}", "Auth\ResetPasswordController@showResetForm")->name("password.reset");
 Route::post("password/reset", "Auth\ResetPasswordController@reset")->name("password.update");
 
-Route::post("profile/", "Auth\UserController@showDashboard")->name("profile");
+Route::get("profile/", "Auth\UserController@showDashboard")->name("profile");
 Route::post("profile/update", "Auth\UserController@updateProfile")->name("profile.update");
 /****************************************************************************/
 
