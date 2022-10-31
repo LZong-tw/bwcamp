8303411d86681b8530ddea048a4066ff9ed6d3a0
Minuet <menuett@gmail.com>
2022-10-31 17:16:00 +0800

    Batch: add num_groups; adjust layout.

--- resources/views/backend/camp/addBatch.blade.php
+++ resources/views/backend/camp/addBatch.blade.php
@@ -13,23 +13,24 @@
             color: red;
         }
     </style>
-    <h2>{{ $camp->abbreviation }} 建立梯次</h2>
+    <h2>{{ $camp->abbreviation }} 批次新增梯次</h2>
     <form action="{{ route("addBatch", $camp->id) }}" method="POST">
         @csrf
         <table class="table table-bordered table-hover" id="form">
             <tr>
-                <th width="82px">名稱</th>
-                <th width="65px">錄取編號前綴</th>
-                <th>梯次開始日</th>
-                <th>梯次結束日</th>
-                <th>允許前台報名</th>
-                <th width="90px">是否延後截止報名</th>
-                <th>報名延後截止日</th>
-                <th>地點名</th>
-                <th>地點</th>
-                <th>報到日</th>
-                <th>電話</th>
-                <th>刪除</th>
+                <th scope="col" class="text-nowrap">名稱</th>
+                <th scope="col" class="text-nowrap">錄取編號<br>前綴</th>
+                <th scope="col" class="text-nowrap">梯次開始日</th>
+                <th scope="col" class="text-nowrap">梯次結束日</th>
+                <th scope="col" class="text-nowrap">允許前台<br>報名</th>
+                <th scope="col" class="text-nowrap">是否延後<br>截止報名</th>
+                <th scope="col" class="text-nowrap">報名延後截止日</th>
+                <th scope="col" class="text-nowrap">報到日</th>
+                <th scope="col" class="text-nowrap">地點</th>
+                <th scope="col" class="text-nowrap">地址</th>
+                <th scope="col" class="text-nowrap">電話</th>
+                <th scope="col" class="text-nowrap">學員組數</th>
+                <th scope="col" class="text-nowrap">新增或<br>刪除</th>
             </tr>
             <tr>
                 <td>
@@ -60,24 +61,27 @@
                     <input type="date" name="late_registration_end[]" id="" class="form-control">
                 </td>
                 <td>
+                    <input type="date" name="check_in_day[]" id="" class="form-control">
+                </td>
+                <td>
                     <input type="text" name="locationName[]" id="" class="form-control">
                 </td>
                 <td>
                     <input type="text" name="location[]" id="" class="form-control">
                 </td>
                 <td>
-                    <input type="date" name="check_in_day[]" id="" class="form-control">
-                </td>
-                <td>
                     <input type="text" name="tel[]" id="" class="form-control">
                 </td>
                 <td>
-                    －
+                    <input type="number" name="num_groups[]" id="" class="form-control">
+                </td>
+                <td>
+                    <a href="#" class="btn btn-primary float-right" onclick="addRow()">+</a>
                 </td>
             </tr>
         </table>
-        <a href="#" class="btn btn-primary float-right" onclick="addRow()">新增梯次</a>
-        <input type="submit" class="btn btn-success" value="送出">
+        <!--a href="#" class="btn btn-primary float-right" onclick="addRow()">新增梯次</a-->
+        <input type="submit" class="btn btn-success" value="確認送出">
     </form>
     <script>
         let ele = ` <tr>  
@@ -109,16 +113,19 @@
                             <input type="date" name="late_registration_end[]" id="" class="form-control">  
                         </td>  
                         <td>  
+                            <input type="date" name="check_in_day[]" id="" class="form-control">  
+                        </td>  
+                        <td>  
                             <input type="text" name="locationName[]" id="" class="form-control">  
                         </td>  
                         <td>  
                             <input type="text" name="location[]" id="" class="form-control">  
                         </td>  
                         <td>  
-                            <input type="date" name="check_in_day[]" id="" class="form-control">  
+                            <input type="text" name="tel[]" id="" class="form-control">  
                         </td>  
                         <td>  
-                            <input type="text" name="tel[]" id="" class="form-control">  
+                            <input type="number" name="num_groups[]" id="" class="form-control">  
                         </td>  
                         <td> 
                             <a href="#" class="btn btn-danger" onclick="this.parentNode.parentNode.remove()">Ｘ</a>
