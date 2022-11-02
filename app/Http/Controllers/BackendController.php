44ff3fc03960d442d2e284b4997ec49d55de03a3
sundaymoonstar <96569282+sundaymoonstar@users.noreply.github.com>
2022-11-03 02:18:03 +0800

    complete p. 7,10,16,18,19 spec
    完成純畫面
    不包含功能

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -812,6 +812,7 @@ class BackendController extends Controller {
                     ->with('fullName', $this->campFullData->fullName)
                     ->with('is_vcamp', 1)
                     ->with('is_care', 0)
+                    ->with('is_careV', 0)
                     ->with('is_ingroup', 0)
                     ->with('groupName', '');
         }
@@ -826,6 +827,7 @@ class BackendController extends Controller {
                     ->with('fullName', $this->campFullData->fullName)
                     ->with('is_vcamp', 1)
                     ->with('is_care', 1)
+                    ->with('is_careV', 0)
                     ->with('is_ingroup', 0)
                     ->with('groupName', '');
         }
@@ -841,6 +843,7 @@ class BackendController extends Controller {
                     ->with('fullName', $this->campFullData->fullName)
                     ->with('is_vcamp', 0)
                     ->with('is_care', 0)
+                    ->with('is_careV', 0)
                     ->with('is_ingroup', 0)
                     ->with('groupName', '');
         }
@@ -855,6 +858,7 @@ class BackendController extends Controller {
                     ->with('fullName', $this->campFullData->fullName)
                     ->with('is_vcamp', 0)
                     ->with('is_care', 1)
+                    ->with('is_careV', 0)
                     ->with('is_ingroup', 0)
                     ->with('groupName', '');
         }
@@ -869,6 +873,22 @@ class BackendController extends Controller {
                     ->with('fullName', $this->campFullData->fullName)
                     ->with('is_vcamp', 0)
                     ->with('is_care', 1)
+                    ->with('is_careV', 0)
+                    ->with('is_ingroup', 1)
+                    ->with('groupName', '第1組');
+        }
+
+        if($request->input('page') == 19) {
+            $this->campFullData->table = 'ceocamp';
+            $columns_zhtw = config('camps_fields.display.' . $this->campFullData->table);
+            return view('backend.in_camp.attendeeList')
+                    ->with('applicants', $applicants)
+                    ->with('batches', $batches)
+                    ->with('columns_zhtw', $columns_zhtw)
+                    ->with('fullName', $this->campFullData->fullName)
+                    ->with('is_vcamp', 0)
+                    ->with('is_care', 1)
+                    ->with('is_careV', 1)
                     ->with('is_ingroup', 1)
                     ->with('groupName', '第1組');
         }
@@ -876,6 +896,105 @@ class BackendController extends Controller {
         $columns_zhtw = config('camps_fields.display.' . $this->campFullData->table);
 
         return view('backend.in_camp.attendeeList')
+                ->with('applicants', $applicants)
+                ->with('batches', $batches)
+                ->with('is_vcamp', strpos($this->campFullData->table, 'vcamp'))
+                ->with('is_care', 0)
+                ->with('is_careV', 0)
+                ->with('is_ingroup', 0)
+                ->with('groupName', '')
+                ->with('columns_zhtw', $columns_zhtw)
+                ->with('fullName', $this->campFullData->fullName);
+    }
+
+    public function showAttendeeAssign(Request $request) {
+        ini_set('max_execution_time', -1);
+        ini_set("memory_limit", -1);
+        $batches = Batch::where("camp_id", $this->campFullData->id)->get();
+        $query = Applicant::select("applicants.*", $this->campFullData->table . ".*", "batchs.name as   bName", "applicants.id as sn", "applicants.created_at as applied_at")
+                        ->join('batchs', 'batchs.id', '=', 'applicants.batch_id')
+                        ->join('camps', 'camps.id', '=', 'batchs.camp_id')
+                        ->join($this->campFullData->table, 'applicants.id', '=', $this->campFullData->table . '.applicant_id')
+                        ->where('camps.id', $this->campFullData->id)->withTrashed();
+        $applicants = $query->get();
+        if (auth()->user()->getPermission(false)->role->level <= 2) {
+        }
+        else if(auth()->user()->getPermission(true, $this->campFullData->id)->level > 2){
+            $constraint = auth()->user()->getPermission(true, $this->campFullData->id)->region;
+            $batch = Batch::where('camp_id', $this->campFullData->id)->where('name', 'like', '%' . $constraint . '%')->first();
+            $applicants = $applicants->filter(function ($applicant) use ($constraint, $batch) {
+                if($batch){
+                    return $applicant->region == $constraint || $applicant->batch_id == $batch->id;
+                }
+                return $applicant->region == $constraint;
+            });
+        }
+
+        if ($request->download) {
+            return \PDF::loadView('backend.in_camp.attendeePhotoPDF', compact('applicants', 'batches'))->download(Carbon::now()->format('YmdHis') . $this->campFullData->table . '義工名冊.pdf');
+        }
+
+        // 設定義工
+        if($request->input('page') == 7) {
+            $this->campFullData->table = 'ceovcamp';
+            $columns_zhtw = config('camps_fields.display.' . $this->campFullData->table);
+            return view('backend.in_camp.attendeeAssign')
+                    ->with('applicants', $applicants)
+                    ->with('batches', $batches)
+                    ->with('columns_zhtw', $columns_zhtw)
+                    ->with('fullName', $this->campFullData->fullName)
+                    ->with('is_vcamp', 1)
+                    ->with('is_care', 0)
+                    ->with('is_ingroup', 0)
+                    ->with('groupName', '');
+        }
+
+        if($request->input('page') == 10) {
+            $this->campFullData->table = 'ceovcamp';
+            $columns_zhtw = config('camps_fields.display.' . $this->campFullData->table);
+            return view('backend.in_camp.attendeeAssign')
+                    ->with('applicants', $applicants)
+                    ->with('batches', $batches)
+                    ->with('columns_zhtw', $columns_zhtw)
+                    ->with('fullName', $this->campFullData->fullName)
+                    ->with('is_vcamp', 1)
+                    ->with('is_care', 1)
+                    ->with('is_ingroup', 0)
+                    ->with('groupName', '');
+        }
+
+        // 設定學員
+        if($request->input('page') == 16) {
+            $this->campFullData->table = 'ceocamp';
+            $columns_zhtw = config('camps_fields.display.' . $this->campFullData->table);
+            return view('backend.in_camp.attendeeAssign')
+                    ->with('applicants', $applicants)
+                    ->with('batches', $batches)
+                    ->with('columns_zhtw', $columns_zhtw)
+                    ->with('fullName', $this->campFullData->fullName)
+                    ->with('is_vcamp', 0)
+                    ->with('is_care', 0)
+                    ->with('is_ingroup', 0)
+                    ->with('groupName', '');
+        }
+
+        if($request->input('page') == 18) {
+            $this->campFullData->table = 'ceocamp';
+            $columns_zhtw = config('camps_fields.display.' . $this->campFullData->table);
+            return view('backend.in_camp.attendeeAssign')
+                    ->with('applicants', $applicants)
+                    ->with('batches', $batches)
+                    ->with('columns_zhtw', $columns_zhtw)
+                    ->with('fullName', $this->campFullData->fullName)
+                    ->with('is_vcamp', 0)
+                    ->with('is_care', 1)
+                    ->with('is_ingroup', 1)
+                    ->with('groupName', '第1組');
+        }
+
+        $columns_zhtw = config('camps_fields.display.' . $this->campFullData->table);
+
+        return view('backend.in_camp.attendeeAssign')
                 ->with('applicants', $applicants)
                 ->with('batches', $batches)
                 ->with('is_vcamp', strpos($this->campFullData->table, 'vcamp'))
