89e36424cc99b8c69f9ce09a5c43983147b238df
LZong <lzong.tw@gmail.com>
2022-11-26 05:13:06 +0800

    Fetch applicant data bug fixed.

--- resources/views/backend/findAccounting.blade.php
+++ resources/views/backend/findAccounting.blade.php
@@ -3,7 +3,7 @@
     <h2>{{ $campFullData->abbreviation }} 現場手動繳費 / 修改繳費資料</h2>
     <form action="{{ route("showCandidate", $campFullData->id) }}" method="post" class="form-horizontal">
         @csrf
-        請輸入報名序號或錄取序號：<input type="text" name="snORadmittedSN" class="form-control" placeholder="">
+        請輸入報名序號或錄取序號：<input type="text" name="snORadmittedSNorName" class="form-control" placeholder="">
         <br>
         <input type="submit" class="btn btn-success" value="送出">
     </form>
