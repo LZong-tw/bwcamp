562bf3eea7d6a3fbc8d0e516cd290d55f21ece92
LZong <lzong.tw@gmail.com>
2022-12-01 04:50:41 +0800

    Search function variant 2.

--- resources/views/components/general/search-component.blade.php
+++ resources/views/components/general/search-component.blade.php
@@ -2,12 +2,11 @@
     <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
     <form action="" method="post">
             @foreach ($columns as $key => $column)
-                @if(($loop->iteration) % 3 == 1) <div class="input-group"> @endif
-                    <span class="input-group-text ml-2">{{ $column['name'] }}</span>
-                    <input type="text" name="condition['{{ $key }}']" id="" class="form-control col-md-2 rounded">
-                @if(($loop->iteration) % 3 == 0 || $loop->last) </div> @endif
+                {{-- @if(($loop->iteration) % 3 == 1) <div class="input-group"> @endif --}}
+                <span class="btn btn-primary mr-2">{{ $column['name'] }}</span>
+                {{-- @if(($loop->iteration) % 3 == 0 || $loop->last) </div> @endif --}}
             @endforeach
-        <div class="input-group">
+        <div class="input-group mt-2">
             <input type="search" name="search" id="" class="form-control col-md-3 rounded" placeholder="搜尋">
             <button type="submit" class="btn btn-success ml-3">搜尋</button>
         </div>
