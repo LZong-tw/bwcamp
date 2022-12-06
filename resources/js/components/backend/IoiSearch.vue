18e3f90ec06735459332f9bbd9c4323e70c42937
LZong <lzong.tw@gmail.com>
2022-12-06 23:41:17 +0800

    Search console push on.

--- resources/js/components/backend/IoiSearch.vue
+++ resources/js/components/backend/IoiSearch.vue
@@ -1,8 +1,8 @@
 <template>
     <form action="" method="post">
-        <span class="btn btn-primary mr-2">123</span>
+        <span class="btn btn-primary mr-2">121231233</span>
         <div class="input-group mt-2">
-            <span class="btn btn-primary mr-2">123</span>
+            <span class="btn btn-primary mr-2">1223</span>
             <input type="search" name="search" id="" class="form-control col-md-3 rounded" placeholder="搜尋">
             <button type="submit" class="btn btn-success ml-3">搜尋</button>
         </div>
@@ -11,7 +11,15 @@
 
 <script>
 export default {
-    name: "IoiSearch"
+    name: "IoiSearch",
+    data() {
+        return {
+            search: "",
+        };
+    },
+    mounted() {
+        console.log("IoiSearch mounted");
+    },
 }
 </script>
 
