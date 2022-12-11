9e8f37f916322162cca1a653fc404ab937456b1d
Minuet <menuett@gmail.com>
2022-12-12 01:03:33 +0800

    utcamp admittedMail

--- app/Jobs/SendAdmittedMail.php
+++ app/Jobs/SendAdmittedMail.php
@@ -27,6 +27,34 @@ class SendAdmittedMail implements ShouldQueue, ShouldBeUnique
     {
         //
         $this->applicant = \App\Models\Applicant::find($applicant_id);
+        //大專教師營
+        if ($this->applicant->batch->camp->table == 'utcamp') {
+            $this->applicant->xgroup = $this->applicant->group;
+            $this->applicant->xnumber = $this->applicant->number;
+            if (strpos($this->applicant->group,'B') !== false) {
+                $this->applicant->xsession = '桃園場';
+                $this->applicant->xaddr = '桃園市中壢區成章四街120號';
+            } elseif (strpos($this->applicant->group,'C') !== false) {
+                $this->applicant->xsession = '新竹場';
+                $this->applicant->xaddr = '新竹縣新豐鄉新興路1號';
+            } elseif (strpos($this->applicant->group,'D') !== false) {
+                $this->applicant->xsession = '台中場';
+                $this->applicant->xaddr = '台中市西區民生路227號';
+            } elseif (strpos($this->applicant->group,'E') !== false) {
+                $this->applicant->xsession = '雲林場';
+                $this->applicant->xaddr = '雲林縣斗六市慶生路6號';
+            } elseif (strpos($this->applicant->group,'F') !== false) {
+                $this->applicant->xsession = '台南場';
+                $this->applicant->xaddr = '台南市東區大學路1號';
+            } elseif (strpos($this->applicant->group,'G') !== false) {
+                $this->applicant->xsession = '高雄場';
+                $this->applicant->xaddr = '高雄市新興區中正四路53號12樓之7';
+            } else {
+                $this->applicant->xsession = '台北場';
+                $this->applicant->xaddr = '台北市南京東路四段165號九樓 福智學堂';
+            }
+            //dd($this->applicant);
+        }
     }
 
     /**
