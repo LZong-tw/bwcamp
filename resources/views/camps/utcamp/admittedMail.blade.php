7b0d6d5c74a340b00e0a2556d8a613d0494276a3
LZong <lzong.tw@gmail.com>
2022-12-13 01:28:35 +0800

    .

--- resources/views/camps/utcamp/admittedMail.blade.php
+++ resources/views/camps/utcamp/admittedMail.blade.php
@@ -11,8 +11,33 @@
         text-align: right;
     }
 </style>
-    <h2 class="center">2023第31屆教師生命成長營</h2> 
-    <h2 class="center">大專教職員梯 {{ $applicant->xsession }} 錄取通知單</h2> 
+@php
+    $group = $applicant->group;
+    if (str_contains($group,'B') !== false) {
+        $applicant->xsession = '桃園場';
+        $applicant->xaddr = '桃園市中壢區成章四街120號';
+    } elseif (str_contains($group,'C') !== false) {
+        $applicant->xsession = '新竹場';
+        $applicant->xaddr = '新竹縣新豐鄉新興路1號';
+    } elseif (str_contains($group,'D') !== false) {
+        $applicant->xsession = '台中場';
+        $applicant->xaddr = '台中市西區民生路227號';
+    } elseif (str_contains($group,'E') !== false) {
+        $applicant->xsession = '雲林場';
+        $applicant->xaddr = '雲林縣斗六市慶生路6號';
+    } elseif (str_contains($group,'F') !== false) {
+        $applicant->xsession = '台南場';
+        $applicant->xaddr = '台南市東區大學路1號';
+    } elseif (str_contains($group,'G') !== false) {
+        $applicant->xsession = '高雄場';
+        $applicant->xaddr = '高雄市新興區中正四路53號12樓之7';
+    } else {
+        $applicant->xsession = '台北場';
+        $applicant->xaddr = '台北市南京東路四段165號九樓 福智學堂';
+    }
+@endphp
+    <h2 class="center">2023第31屆教師生命成長營</h2>
+    <h2 class="center">大專教職員梯 {{ $applicant->xsession }} 錄取通知單</h2>
 <table width="100%" style="table-layout:fixed; border: 0;">
     <tr>
         <td>報名序號：{{ $applicant->id }}</td>
@@ -32,5 +57,5 @@
     若有任何問題，歡迎來電(02)7714-6066 分機20318 邱先生。<br>
     12/12~15將有營隊關懷員與您聯絡，提供營隊相關資訊。<br>
 <br>
-<a class="right">財團法人福智文教基金會　謹此</a><br> 
+<a class="right">財團法人福智文教基金會　謹此</a><br>
 <a class="right">{{ \Carbon\Carbon::now()->year }}  年　{{ \Carbon\Carbon::now()->month }}  月 　 {{ \Carbon\Carbon::now()->day }}  日</a>
