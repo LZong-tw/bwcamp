6dbd4f64f355ceab1086ee3e9c0834465be229c6
LZong <lzong.tw@gmail.com>
2022-12-13 00:05:50 +0800

    Tcamp admission result batch display modified.

--- resources/views/camps/tcamp/admissionResult.blade.php
+++ resources/views/camps/tcamp/admissionResult.blade.php
@@ -10,12 +10,12 @@
     </div>
     <div class="row">
         @php
-            $skip = false;   
+            $skip = false;
         @endphp
         @if($camp_data->variant == "utcamp" && now()->lt("2021-11-30"))
             @php
                 $skip = true;
-            @endphp        
+            @endphp
             <div class="alert alert-success" role="alert">
                 您好，感謝您報名 【2022第30屆教師生命成長營 大專教職員梯】，大會因故需調整錄取時程，將修正為：11/30(二)、12/20(一)、1/5(三)，將於以上時間點寄發「錄取通知」電子信件(email)。再次感謝您的報名！
             </div>
@@ -37,7 +37,7 @@
                                         <td><h6>組別</h6></td>
                                     </tr>
                                     <tr>
-                                        <td>{{ $applicant->region }}</td>
+                                        <td>{{ $applicant->batch->name }}</td>
                                         <td>{{ $applicant->name }}</td>
                                         <td><u>{{ $applicant->group }}{{ $applicant->number }}</u></td>
                                         <td><u>{{ $applicant->group }}</u></td>
@@ -111,7 +111,7 @@
                         <div class="card-header">
                             <h4>查詢結果</h4>
                         </div>
-                        <div class="card-body">                                
+                        <div class="card-body">
                             <p class="card-text">
                                 敬愛的教育夥伴 {{ $applicant->name }} ，您好！ <br><br>
                                 　　「教師生命成長營」自舉辦以來，每年都得到教育夥伴們的支持和肯定，思及社會上仍有這麼多人共同關心莘莘學子們的學習成長，令人深感振奮！每一位老師的報名都是鼓舞我們的一分力量，激勵基金會全體人員持續不懈，與大家共同攜手為教育盡心盡力。 <br><br>
@@ -120,9 +120,9 @@
                                 　　關注「福智文教基金會」網站：<a href="https://bwfoce.org" target="_blank" rel="noopener noreferrer">https://bwfoce.org</a><br>
                                 　　關注「幸福心學堂online」FB社團：<a href="https://www.facebook.com/groups/bwfoce.happiness.new/" target="_blank" rel="noopener noreferrer">https://www.facebook.com/groups/bwfoce.happiness.new/</a><br>
                                 <br>
-                                祝福　教安，健康平安！ 
+                                祝福　教安，健康平安！
                             </p>
-                            <a class="right">財團法人福智文教基金會　敬啟</a><br> 
+                            <a class="right">財團法人福智文教基金會　敬啟</a><br>
                             <a class="right">{{ \Carbon\Carbon::now()->year }}  年　{{ \Carbon\Carbon::now()->month }}  月  {{ \Carbon\Carbon::now()->day }}  日</a>
                             <br><br>
                             <input type='button' class='btn btn-warning' value='回上一頁' onclick=self.history.back()>
@@ -130,7 +130,7 @@
                         </div>
                     </div>
                 </div>
-            @endif    
+            @endif
         @endif
     </div>
 @stop
