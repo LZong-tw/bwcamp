dc2c0612ab128ffad1c28f01310b699c74db91c3
LZong <lzong.tw@gmail.com>
2022-12-24 15:57:24 +0800

    Dynamic selection interface completed.

--- resources/js/components/backend/IoiSearch.vue
+++ resources/js/components/backend/IoiSearch.vue
@@ -15,14 +15,12 @@
                 <td v-for="(item, key) in columns"
                     :key="key"
                     :id="key"
-                    @click="toggleData(key)"
                     class="border-0">
                 </td>
             </tr>
         </table>
         <div class="input-group mt-2">
-            <input type="search" name="search" id="" class="form-control col-md-3 rounded" placeholder="搜尋">
-            <button type="submit" class="btn btn-success ml-3">搜尋</button>
+            <button type="submit" class="btn btn-success ml-3">送出</button>
         </div>
     </form>
 </template>
@@ -32,25 +30,64 @@ export default {
     name: "IoiSearch",
     data() {
         return {
-            search: "",
+            search: [],
             columns: window.columns,
             theData: window.theData,
+            originalData: window.theData,
         };
     },
     methods: {
+        filterSearch(column) {
+            let search = $(`#search${column.id}`).val();
+            this.search[column.id] = search;
+            this.theData = this.originalData;
+            this.theData = this.theData.filter((item) => {
+                return item[column.id].toLowerCase().includes(search.toLowerCase());
+            });
+            if (this.theData.length == 0) {
+                this.theData = this.theData = this.originalData;
+            }
+            this.toggleData(column.id);
+            this.toggleData(column.id);
+            $(`#search${column.id}`).focus();
+        },
         toggleData(id) {
             this.columns[id].show = !this.columns[id].show;
             if (this.columns[id].show) {
                 let table = document.createElement("table");
-                let tr = document.createElement("tr");
-                let td = document.createElement("td");
                 this.theData.forEach((item, key) => {
-                    td.innerHTML = item[id] ? item[id] : "";
-                    tr.appendChild(td);
-                    table.appendChild(tr);
+                    if (table.innerHTML.includes(item[id])) {
+                        return;
+                    }
+                    if (item[id]) {
+                        if (key == 0) {
+                            let tr_1 = document.createElement("tr");
+                            let td_1 = document.createElement("td");
+                            let text = document.createElement("input");
+                            text.setAttribute("type", "text");
+                            text.setAttribute("placeholder", "搜尋");
+                            text.setAttribute("class", "form-control");
+                            text.setAttribute("id", "search" + id);
+                            text.setAttribute("oninput", "window.vueComponent.filterSearch(" + id + ")");
+                            text.setAttribute("value", window.vueComponent.$data.search[id]);
+                            td_1.appendChild(text);
+                            tr_1.appendChild(td_1);
+                            table.appendChild(tr_1);
+                        }
+                        let tr = document.createElement("tr");
+                        tr.setAttribute("id", "tr" + id + "key" + key);
+                        let td = document.createElement("td");
+                        let checkbox = document.createElement("input");
+                        checkbox.type = "checkbox";
+                        checkbox.name = "value[" + id +"][" + key + "]";
+                        td.appendChild(checkbox);
+                        td.innerHTML += item[id];
+                        tr.appendChild(td);
+                        table.appendChild(tr);
+                    }
                 });
                 $("#" + id).append(`<div id="show-` + id +`">
-                                        ` + table.innerHTML + `
+                                        ` + table.outerHTML + `
                                     </div>`);
             } else {
                 $("#show-" + id).remove();
@@ -58,7 +95,10 @@ export default {
         },
     },
     mounted() {
-        // console.log(this.columns);
+        window.vueComponent = this;
+        for (let key in this.theData[0]) {
+            this.search[key] = "";
+        }
     },
 }
 </script>
