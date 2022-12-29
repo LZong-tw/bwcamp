7cf6361fcb7b88f0e98c39995be3531bc446e767
sundaymoonstar <96569282+sundaymoonstar@users.noreply.github.com>
2022-12-29 16:45:24 +0800

    Fixed bug from theList.blade.php
    修正切換筆數無作用的問題

--- resources/views/components/button/options.blade.php
+++ resources/views/components/button/options.blade.php
@@ -1,6 +1,6 @@
 <div>
     <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
-    <p align="right" style="float: right;">
+    <p align="right">
         <a href="{{ route("showLearners", $campFullData->id) }}?download=1" target="_blank" rel="noopener noreferrer" class="btn btn-danger mb-3">匯出資料</a>
         @if($isIngroup)
             @if(!$isCareV)
@@ -17,7 +17,7 @@
             @if($isCare)                &nbsp;&nbsp;
                 <a href="{{ route("showRegistration", $campFullData->vcamp->id) }}" rel="noopener noreferrer" class="btn btn-danger mb-3" target="_blank">新增義工</a>
                 <a href="?isSetting=1&batch_id={{ $currentBatch?->id ?? "" }}" class="btn btn-danger mb-3">設定組別/職務</a>
-            @endif            &nbsp;&nbsp;
+            @endif
         @endif
     </p>
 </div>
