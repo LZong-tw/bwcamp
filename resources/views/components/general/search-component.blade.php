9faff5e0619a914903c1ddd8344b0c4d9dbd7e7e
LZong <lzong.tw@gmail.com>
2022-12-29 15:31:16 +0800

    Bug fixed.

--- resources/views/components/general/search-component.blade.php
+++ resources/views/components/general/search-component.blade.php
@@ -3,7 +3,7 @@
     <div class="alert-primary mb-3 border border-secondary rounded col-8 py-2">
         <span>查詢條件：{{ $queryStr ?? "無" }}</span>
     </div>
-    @if($camp->table != "ceocamp" || $isCare)
+    @if($camp->table != "ceocamp" || $isCareV)
         <ioi-search></ioi-search>
     @else
         @php
