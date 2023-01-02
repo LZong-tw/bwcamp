3c6568e10364ce05e1110435f8a5d4d49e9bbb09
LZong <lzong.tw@gmail.com>
2023-01-02 23:08:27 +0800

    Bug fixed.

--- app/Http/Controllers/CheckInController.php
+++ app/Http/Controllers/CheckInController.php
@@ -71,7 +71,7 @@ class CheckInController extends Controller {
                 ->orWhere(\DB::raw("replace(mobile, ')', '')"), 'like', '%' . $request->query_str . '%')
                 ->orWhere(\DB::raw("replace(mobile, '（', '')"), 'like', '%' . $request->query_str . '%')
                 ->orWhere(\DB::raw("replace(mobile, '）', '')"), 'like', '%' . $request->query_str . '%');
-            })->orderBy('number', 'asc')->get();
+            })->get()->sortBy(['batch.camp.id', 'batch.id', 'groupRelation.alias', 'numberRelation.number']);
         $batches = $applicants->pluck('batch.name', 'batch.id')->unique();
         $request->flash();
         return view('checkIn.home', compact('applicants', 'batches'));
