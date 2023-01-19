283d22cacdd79763c330cec90a483e8eddf0bd5f
LZong <lzong.tw@gmail.com>
2023-01-19 14:47:14 +0800

    Bug fixed.

--- app/Services/BackendService.php
+++ app/Services/BackendService.php
@@ -173,21 +173,19 @@ class BackendService
                 ];
             }
             elseif ($entity["uses_user_id"] == "generation_needed") {
+                (new UserApplicantXref([
+                    'user_id' => $user->id,
+                    'applicant_id' => $entity["id"],
+                ]))->save();
+                $applicant = Applicant::findOrFail($entity["id"]);
+                $applicant->is_admitted = 1;
+                $applicant->save();
                 $user = $this->generateUser($applicant);
                 (new OrgUser([
                     'org_id' => $groupOrg->id,
                     'user_id' => $user->id,
                     'user_type' => 'App\Models\User',
                 ]))->save();
-                if ($entity["type"] == "applicant") {
-                    (new UserApplicantXref([
-                        'user_id' => $user->id,
-                        'applicant_id' => $entity["id"],
-                    ]))->save();
-                    $applicant = Applicant::findOrFail($entity["id"]);
-                    $applicant->is_admitted = 1;
-                    $applicant->save();
-                }
                 $succeedList[] = [
                     'applicant' => $applicant,
                     'connected_to_user' => $user,
