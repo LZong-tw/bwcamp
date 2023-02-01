aa0e45f562b1158d68f7ee0514cf0fa2e3e07326
LZong <lzong.tw@gmail.com>
2023-02-02 03:15:42 +0800

    Bug fixed.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
                     @if(auth()->user()->getPermission()->level <= 5 || \App\Models\OrgUser::where('user_id', auth()->user()->id)->count() > 0)
                         <a href="{{ route("backendIndex") }}" class="btn btn-success">後台首頁</a>
                     @endif
-                    @if(auth()->user()->getPermission()->level <= 6)
+                    @if(auth()->user()->getPermission()->level <= 6 || \App\Models\OrgUser::where('user_id', auth()->user()->id)->count() > 0)
                         <a href="/checkin?t={{ time() }}" target="_blank" class="btn btn-primary">報到系統</a>
                     @endif
                 </div>
