7782fb3fad2085b7166b8092194d8dd42627302f
LZong <lzong.tw@gmail.com>
2023-02-16 17:33:26 +0800

    Bug fixed.

--- resources/views/backend/in_camp/attendeeInfo.blade.php
+++ resources/views/backend/in_camp/attendeeInfo.blade.php
@@ -94,10 +94,10 @@
         @if($applicant->groupRelation->count())
             <div class="row">
                 <div class="col-md-8">
-                    {{ $applicant->groupRelation->group->name }}
+                    {{ $applicant->groupRelation->name }}
                 </div>
                 <div class="col-md-4">
-                    <a href="{{ route('deleteApplicantGroup', [$camp->id, "applicant_id" => $applicant->id, "group_id" => $applicant->groupRelation->group->id]) }}" class="btn btn-danger">刪除</a>
+                    <a href="{{ route('deleteApplicantGroup', [$camp->id, "applicant_id" => $applicant->id, "group_id" => $applicant->groupRelation->id]) }}" class="btn btn-danger">刪除</a>
                 </div>
             </div>
         @else
