4ec056ec582f590f3e3f8ea20a68716fc285847d
LZong <lzong.tw@gmail.com>
2023-02-19 00:07:14 +0800

    Bug fixed.

--- resources/views/components/general/settings.blade.php
+++ resources/views/components/general/settings.blade.php
@@ -23,9 +23,11 @@
                 <option value=''>- 請選擇 -</option>
                 @forelse($carers as $carer)
                     @php
+                        $query = $carer->groupOrgRelation()->where('position', 'like', '%關懷小組第%');
                         if (request()->batch_id) {
-                            $care = $carer->each(fn($carer) => $carer->groupOrgRelation = $carer->groupOrgRelation->where('batch_id', request()->batch_id)->where('position', 'like', '%關懷小組第%'));
+                            $query = $query->where('batch_id', request()->batch_id);
                         }
+                        $carer->groupOrgRelation = $query->get();
                     @endphp
                     <option value='{{ $carer->id }}'>{{ $carer->groupOrgRelation?->first()?->batch?->name }}：{{ $carer->name }}：{{ $carer->groupOrgRelation->first()?->position }}</option>
                 @empty
