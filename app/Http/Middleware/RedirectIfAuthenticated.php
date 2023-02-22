3b23c8cac3aa46ee431bb8fc1c6bc6172fa6d0d0
LZong <lzong.tw@gmail.com>
2023-02-22 23:46:27 +0800

    Modified.

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,6 +19,13 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
+            if (auth()->user()->roles()->filter(static fn ($r) => $r->camp->year == now()->year)->count() == 1) {
+                foreach (auth()->user()->roles as $role) {
+                    if ($role->camp->year == now()->year && str_contains($role->position, "關懷小組") && str_contains($role->position, "組員")) {
+                        $this->redirectTo = route("showLearners", $role->camp->id);
+                    }
+                }
+            }
             return redirect(RouteServiceProvider::HOME);
         }
 
