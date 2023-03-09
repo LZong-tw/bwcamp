1f2daf86f9b195f1356826049888b2374173d8d9
LZong <lzong.tw@gmail.com>
2023-03-09 15:25:11 +0800

    New permissions table modified.

---
+++ database/migrations/2023_03_09_152007_permissions_add_column.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        if (!Schema::hasColumn('permissions', 'camp_id')) {
+            Schema::table('permissions', function (Blueprint $table) {
+                $table->unsignedBigInteger('camp_id')->nullable()->after('id');
+            });
+        }
+    }
+};
