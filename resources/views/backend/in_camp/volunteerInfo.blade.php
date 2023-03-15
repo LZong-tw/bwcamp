9cfad54b0c178f7d10129766c8771bfd60a5de35
LZong <lzong.tw@gmail.com>
2023-03-15 10:47:15 +0800

    Bug fixed.

--- resources/views/backend/in_camp/volunteerInfo.blade.php
+++ resources/views/backend/in_camp/volunteerInfo.blade.php
@@ -25,7 +25,7 @@
     <h4>{{ $camp->fullName }}>>義工詳細資料>>{{ $applicant->name }}</h4>
 
     <!-- 修改學員資料,使用報名網頁 -->
-    @if(auth()->user()->can("\App\Models\Volunteer.update"))
+    @if(\App\Models\User::find(auth()->user()->id)->can("\App\Models\Volunteer.update"))
         <form target="_blank" action="{{ route('queryupdate', $batch->id) }}" method="post">
             @csrf
             <input type="hidden" name="sn" value="{{ $applicant->applicant_id }}">
