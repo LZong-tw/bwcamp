b7597398bb33756aeec641c50ad4ac61673f152b
LZong <lzong.tw@gmail.com>
2023-04-09 03:39:33 +0800

    #3vhy4jh

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -1028,33 +1028,11 @@ class BackendController extends Controller {
     }
 
     public function showLearners(Request $request) {
-//        if (!$this->authorize('viewAny', [auth()->user(), Applicant::class])) {
-//            return "<h3>沒有權限：瀏覽所有學員</h3>";
-//        }
         $user = \App\Models\User::findOrFail(auth()->user()->id);
         view()->share('user', $user);
         $batches = Batch::where("camp_id", $this->campFullData->id)->get();
-        // todo: permission resolver: 找出權限可達範圍的聯集
-//        if (!($user->isAbleTo("\App\Models\CarerApplicantXref.create") || $user->isAbleTo("\App\Models\CarerApplicantXref.assign"))
-//                && $user->id != 1) {
-//            return "<h3>沒有權限：設定學員關懷員</h3>";
-//        }
-//        elseif($user->isAbleTo("\App\Models\CarerApplicantXref.create") || $user->isAbleTo("\App\Models\CarerApplicantXref.assign")) {
-//            // 小組長只能看自己被賦予的梯次
-//            $batches = $batches->filter(static fn($batch) => $user->roles->filter(function ($role) use ($batch) {
-//                return $role->batch_id == $batch->id;
-//            })->count() > 0);
-//            if (!$request->batch_id) {
-//                $batch_id = $batches->first()->id;
-//                return redirect()->route('showLearners', [
-//                    'camp_id' => $this->campFullData->id,
-//                    'batch_id' => $batch_id,
-//                    'isSettingCarer' => $request->isSettingCarer]);
-//            }
-//        }
 
-        if (!$user->isAbleTo('\App\Models\Applicant.read') && $user->id != 1) {
-            // todo: 要檢查到營隊
+        if (!$user->canAccessResource(new \App\Models\Applicant, 'read', $this->campFullData) && $user->id != 1) {
             return "<h3>沒有權限：瀏覽任何學員</h3>";
         }
         ini_set('max_execution_time', -1);
--- resources/views/backend/camp/modifyOrg.blade.php
+++ resources/views/backend/camp/modifyOrg.blade.php
@@ -17,6 +17,17 @@
     <form action="{{ route("modifyOrg", [$camp->id, $org->id]) }}" method="POST">
         @csrf
         <div class='row form-group'>
+            <label for='inputPos' class='col-md-2 control-label'>梯次</label>
+            <div class='col-md-6'>
+                <select name="batch_id" id="" class="form-control">
+                    <option value="">不限</option>
+                    @foreach($camp->batches ?? [] as $batch)
+                        <option value="{{ $batch->id }}" {{ $batch->id == $org->batch_id ? "selected" : "" }}>{{ $batch->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class='row form-group'>
             <label for='inputSec' class='col-md-2 control-label'>大組名稱</label>
             @if($org->position == 'root')
             <div class='col-md-6'>
@@ -31,10 +42,25 @@
         </div>
         @if($org->position != 'root')
         <div class='row form-group'>
-            <label for='inputPos' class='col-md-2 control-label'>職務名稱</label>
+            <label for='inputPos' class='col-md-2 control-label'>小組及職務名稱</label>
             <div class='col-md-6'>
                 <input type="string" name="position" id="" class='form-control' value="{{ $org->position ?? "" }}">
             </div>
+        </div>
+        <div class='row form-group'>
+            <label for='inputPos' class='col-md-2 control-label'>綁定的學員組別</label>
+            <div class='col-md-6'>
+                <select name="group_id" id="" class="form-control">
+                    <option value="">無</option>
+                    @foreach($camp->groups ?? [] as $group)
+                        <option value="{{ $group->id }}" {{ $group->id == $org->group_id ? "selected" : "" }}>{{ $group->alias }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class='row form-group'>
+        </div>
+        <div class='row form-group'>
             <div class='ml-3 mt-3'>
                 @include('backend.camp.permission_table')
             </div>
