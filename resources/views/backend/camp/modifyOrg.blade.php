d1b356358e32a8c83cd77a8f06ec03a688447bfd
LZong <lzong.tw@gmail.com>
2023-04-09 04:53:30 +0800

    Bug fixed, code moved.

--- resources/views/backend/camp/modifyOrg.blade.php
+++ resources/views/backend/camp/modifyOrg.blade.php
@@ -71,44 +71,4 @@
         <input type="submit" class="btn btn-success" value="確認修改">
         <a href="{{ route('showOrgs', $camp->id) }}" class="btn btn-danger">取消修改</a>
     </form>
-    <script>
-        (function () {
-            let checkboxes = document.getElementsByClassName("checkbox");
-            let radios = document.getElementsByClassName("radio");
-            for (let i = 0; i < checkboxes.length; i++) {
-                checkboxes[i].checked && checkboxes[i].parentNode.classList.add("bg-success");
-                checkboxes[i].addEventListener("change", function () {
-                    if (this.checked) {
-                        this.parentNode.classList.add("bg-success");
-                    }
-                    else {
-                        let checked_actions = 0;
-                        document.getElementsByName(this.name).forEach(item => checked_actions ||= item.checked);
-                        if (!checked_actions) {
-                            for (let i = 0; i < radios.length; i++) {
-                                let action = this.name.replace("[", "").replace("]", "").replace("[", "").replace("]", "").replace("resources", "");
-                                let range = radios[i].name.replace("[", "").replace("]", "").replace("range", "");
-                                if (range == action) {
-                                    radios[i].checked = false;
-                                    radios[i].parentNode.classList.remove("bg-success");
-                                }
-                            }
-                        }
-                        this.parentNode.classList.remove("bg-success");
-                    }
-                });
-            }
-            for (let i = 0; i < radios.length; i++) {
-                radios[i].checked && radios[i].parentNode.classList.add("bg-success");
-                radios[i].addEventListener("change", function () {
-                    if (this.checked) {
-                        this.parentNode.classList.add("bg-success");
-                    }
-                    else {
-                        this.parentNode.classList.remove("bg-success");
-                    }
-                });
-            }
-        })();
-    </script>
 @endsection
--- resources/views/backend/camp/permission_table.blade.php
+++ resources/views/backend/camp/permission_table.blade.php
@@ -71,3 +71,46 @@
         @endforeach
     </tbody>
 </table>
+<script>
+    (function () {
+        let checkboxes = document.getElementsByClassName("checkbox");
+        let radios = document.getElementsByClassName("radio");
+        for (let i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].checked && checkboxes[i].parentNode.classList.add("bg-success");
+            checkboxes[i].addEventListener("change", function () {
+                if (this.checked) {
+                    this.parentNode.classList.add("bg-success");
+                }
+                else {
+                    let checked_actions = 0;
+                    document.getElementsByName(this.name).forEach(item => checked_actions ||= item.checked);
+                    if (!checked_actions) {
+                        for (let i = 0; i < radios.length; i++) {
+                            let action = this.name.replace("[", "").replace("]", "").replace("[", "").replace("]", "").replace("resources", "");
+                            let range = radios[i].name.replace("[", "").replace("]", "").replace("range", "");
+                            if (range == action) {
+                                radios[i].checked = false;
+                                radios[i].parentNode.classList.remove("bg-success");
+                            }
+                        }
+                    }
+                    this.parentNode.classList.remove("bg-success");
+                }
+            });
+        }
+        for (let i = 0; i < radios.length; i++) {
+            radios[i].checked && radios[i].parentNode.classList.add("bg-success");
+            radios[i].addEventListener("change", function () {
+                if (this.checked) {
+                    this.parentNode.classList.add("bg-success");
+                    document.getElementsByName(this.name).forEach(item => {
+                        if (item != this) {
+                            item.checked = false;
+                            item.parentNode.classList.remove("bg-success");
+                        }
+                    });
+                }
+            });
+        }
+    })();
+</script>
