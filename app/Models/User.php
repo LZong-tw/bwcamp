7ff7c0fe49eddd50e30d846f8246eeebdbe72b06
LZong <lzong.tw@gmail.com>
2023-04-16 22:38:25 +0800

    Volunteers larger group permission bug fixed.

--- app/Models/User.php
+++ app/Models/User.php
@@ -175,7 +175,10 @@ class User extends Authenticatable
                     return true;
                 // 1: volunteer_large_group
                 case 1:
-                    if (($class == "App\Models\Applicant" || $class == "App\Models\Volunteer") && $resource->user?->roles) {
+                    if ($class == "App\Models\Volunteer" && $resource->roles) {
+                        return $resource->roles->whereIn("section", $this->roles()->where('camp_id', $camp->id)->pluck("section"))->count();
+                    }
+                    if ($class == "App\Models\Applicant" && $resource->user?->roles) {
                         return $resource->user->roles->whereIn("section", $this->roles()->where('camp_id', $camp->id)->pluck("section"))->count();
                     }
                     if (($class == "App\Models\User" || $class == "App\User") && $resource->roles) {
