1252a74117758b3ece734bef9ca1637e40bb4d93
Minuet <menuett@gmail.com>
2023-05-03 09:50:13 +0200

    ycamp update 2023

--- resources/views/camps/ycamp/success.blade.php
+++ resources/views/camps/ycamp/success.blade.php
@@ -12,7 +12,7 @@
             <p class="card-text">
                 恭喜您已完成{{ $camp_data->fullName }}（簡稱本營隊）網路報名程序，您所填寫的個人資料，僅用於此次大專營的報名及活動聯絡之用。財團法人福智文教基金會將依個人資料保護法及相關法令之規定善盡保密責任。<br>
                 請記下您的<span class="text-danger font-weight-bold">《 報名序號：{{ $applicant->id }} 》</span>作為日後查詢使用。<br>
-                錄取名單將於 5/30、6/15、7/5、7/15、7/30 於<a href="{{ $camp_data->site_url }}">營隊官網</a>公佈，請自行上網查詢。<br>
+                錄取名單將於 6/20 於<a href="{{ $camp_data->site_url }}">營隊官網</a>公佈，請自行上網查詢。<br>
                 {{-- 錄取結果將在 <u>{{ $camp_data->admission_announcing_date }} ({{ $admission_announcing_date_Weekday }})</u> 起於網上公佈，請自行上網查詢，<br>
                 並於 <u>{{ $camp_data->admission_confirming_end }} ({{ $admission_confirming_end_Weekday }})</u> 前上網回覆確認參加，倘未回覆，視同放棄。--}}
             </p>
