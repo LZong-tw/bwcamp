70a9dfff0b41b9e956128342c7177b3a5cadcfc3
LZong <lzong.tw@gmail.com>
2023-05-04 13:33:11 +0800

    .

--- resources/views/components/table/applicant-list.blade.php
+++ resources/views/components/table/applicant-list.blade.php
@@ -5,7 +5,7 @@
         <div class="div1">
         </div>
     </div>
-    <div class="text-warning mt-3">
+    <div class="text-danger mt-3">
         已取消報名： {{ $applicants->whereNotNull("deleted_at")->count() }} 人
     </div>
     <table class="table table-bordered table-hover"
