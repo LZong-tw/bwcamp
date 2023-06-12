66542d44b0034ec087012644591d4e17e38abe1e
LZong <lzong.tw@gmail.com>
2023-06-12 16:30:35 +0800

    .

--- resources/views/backend/in_camp/attendeeInfoCeocamp.blade.php
+++ resources/views/backend/in_camp/attendeeInfoCeocamp.blade.php
@@ -197,7 +197,7 @@
                 <br>
                 <hr>
                 <h5>報到條碼</h5>
-                <img src="data:image/png;base64,{{ \DNS2D::getBarcodePNG('{" applicant_id":' . $applicant->id .
+                <img src="data:image/png;base64,{{ \DNS2D::getBarcodePNG('{"applicant_id":' . $applicant->id .
                 '}', 'QRCODE') }}" alt="barcode" height="200px" class="mb-3"/>
                 <br>
             </div>
