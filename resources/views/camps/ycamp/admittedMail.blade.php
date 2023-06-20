62cb9f5bc0069db7ee05a650abfe04dc18314d4b
Minuet <menuett@gmail.com>
2023-06-20 12:39:58 +0800

    fix bugs

--- resources/views/camps/ycamp/admittedMail.blade.php
+++ resources/views/camps/ycamp/admittedMail.blade.php
@@ -14,7 +14,7 @@
 </p>
 <p class="card-text text-indent">此次營隊報名人數超過1,500人，錄取1,100 名，竭誠歡迎您的到來！請詳閱錄取/報到通知，並於6月28日(三)前回覆交通方式！祝福您營隊收穫滿滿。</p>
 <ul>
-    <li><p class="card-text indent"><a href="http://bwcamp.minuet.com/downloads/ycamp2023/【2023第56屆大專青年生命成長營】錄取通知單.pdf">錄取/報到通知連結</a></p></li>
+    <li><p class="card-text indent"><a href="http://bwcamp.bwfoce.org/downloads/ycamp2023/【2023第56屆大專青年生命成長營】錄取通知單.pdf">錄取/報到通知連結</a></p></li>
     <li><p class="card-text indent">上網<a href="https://bwcamp.bwfoce.org/camp/{{ $applicant->batch->id }}/showadmit?sn={{ $applicant->id }}&name={{ $applicant->name }}">回覆交通方式</a></p>
     <p>若以上連結無法點選，請複製下方文字後，再由瀏覽器進入頁面做回覆：</p>
     <p>https://bwcamp.bwfoce.org/camp/{{ $applicant->batch->id }}/queryadmit</p>
