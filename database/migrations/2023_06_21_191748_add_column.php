022b31b51d6c875d4c26efa7568dd1c65d234d0b
LZong <lzong.tw@gmail.com>
2023-06-21 20:36:48 +0800

    Compatibility issue.

--- database/migrations/2023_06_21_191748_add_column.php
+++ database/migrations/2023_06_21_191748_add_column.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('applicants', function (Blueprint $table) {
-            $table->json('files')->nullable()->after('remark');
+            $table->text('files')->nullable()->after('remark');
         });
     }
 };
