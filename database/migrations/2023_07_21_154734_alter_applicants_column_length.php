40d5c5d670327b434a5ceabe21a600d85c102f45
LZong <lzong.tw@gmail.com>
2023-07-21 15:52:08 +0800

    。

---
+++ database/migrations/2023_07_21_154734_alter_applicants_column_length.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('applicants', function (Blueprint $table) {
+            $table->string('name')->change();
+            $table->string('english_name')->change();
+            $table->string('nationality')->change();
+            $table->string('mobile')->change();
+            $table->string('phone_home')->change();
+            $table->string('phone_work')->change();
+            $table->string('fax')->change();
+            $table->string('line')->change();
+            $table->string('wechat')->change();
+            $table->string('email')->change();
+            $table->string('emergency_name')->change();
+            $table->string('emergency_fax')->change();
+            $table->string('emergency_mobile')->change();
+            $table->string('emergency_relationship')->change();
+            $table->string('emergency_phone_home')->change();
+            $table->string('emergency_phone_work')->change();
+            $table->string('introducer_email')->change();
+            $table->string('introducer_name')->change();
+            $table->string('introducer_phone')->change();
+            $table->string('introducer_relationship')->change();
+        });
+    }
+};
