72878797e08c82476bf4c4b7571fb0971b5c08cf
LZong <lzong.tw@gmail.com>
2023-09-02 02:59:11 +0800

    .

--- resources/views/camps/coupon/checkInMail.blade.php
+++ resources/views/camps/coupon/checkInMail.blade.php
@@ -35,7 +35,7 @@
     {{ $applicant->name }}
 </div>
 <div style="margin-left: 90px;margin-top: 85px;font-size: 36px;">
-    {{ $applicant->group }} 排 {{ $applicant->number }} 號
+    {{ $applicant->group }}{{ $applicant->number }} 號
 </div>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 </body>
