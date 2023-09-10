84924e818d9140db73675e9f1ac1743075cf4264
Minuet <menuett@gmail.com>
2023-09-10 13:25:48 +0800

    1.Add tvcamp and utvcamp 2.ycamp: download certificate

---
+++ resources/views/camps/tvcamp/layout.blade.php
@@ -0,0 +1,100 @@
+<html lang='zh-Hant'>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name='description' content='「教育是人類升沈的樞紐」邀請您報名參加教師生命成長營。' />
+    <meta name='author' content='福智文教基金會'>
+    <meta property='og:url' content='http://bwfoce.org/2023tcamp'/>
+    <meta property='og:title' content='{{ $camp_data->abbreviation }}'/>
+    <meta property='og:description' content='「教育是人類升沈的樞紐」邀請您報名參加教師生命成長營。' />
+    <meta property='og:image' content='http://bwfoce.org/2023tcamp'/>
+    {{-- <link rel='icon' href='/camp/favicon.ico'> --}}
+    <title> {{ $camp_data->fullName }} </title>
+    <!-- Bootstrap core CSS -->
+    @vite(['resources/js/app.js'])
+    <link href='{{ asset('css/app.css') }}' rel='stylesheet'>
+    {{-- <link href='{{ asset('css/bootstrap.min.css') }}' rel='stylesheet'> --}}
+    <!-- Custom styles for this template -->
+    <link href='{{ asset('css/camp.css') }}' rel='stylesheet'>
+    <!-- jQuery library-->
+    {{-- <script src='{{ asset('js/jquery-3.5.1.min.js') }}'></script> --}}
+    <script src="{{ asset('js/popper.2.11.6.min.js') }}"></script>
+    <!-- Bootstrap core JS -->
+    {{-- <script src='{{ asset('js/bootstrap.bundle.min.js') }}'></script> --}}
+    <!-- Bootstrap confirmation JS -->
+    <script src='{{ asset('js/bootstrap-confirmation.min.js') }}'></script>
+</head>
+<BODY>
+<div id='fb-root'></div>
+@if(isset($isBackend))
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #f8d7da;">
+        <div class="container" style="color: #721c24">
+            {{ $isBackend }}
+        </div>
+    </nav>
+@else
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #ebfbeb;">
+        <div class="container">
+            <a class='navbar-brand' href=''>{{ $camp_data->abbreviation }}</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    {{-- <li class="nav-item active"> --}}
+                    {{-- <span class="sr-only">(current)</span> --}}
+                    @if(!$camp_data->variant)
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://bwfoce.wixsite.com/2023gtcamp">營隊資訊</a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://bwfoce.wixsite.com/2022utcamp">營隊資訊</a>
+                        </li>
+                    @endif
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('registration', $batch_id) }}">報名表單</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('query', $batch_id) }}">報名查詢/修改</a>
+                    </li>
+                    {{-- <li class="nav-item">
+                        <a class="nav-link" href="{{ route('payment', $batch_id) }}">線上繳費</a>
+                    </li> --}}
+                    @if(\Carbon\Carbon::now() >= \Carbon\Carbon::createFromFormat("Y-m-d", $camp_data->admission_announcing_date))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route("queryadmitGET", $batch_id) }}">錄取查詢</a>
+                        </li>
+                    @endif
+                    @if(!$camp_data->variant)
+                        <li class="nav-item">
+                            <!--<a class="nav-link" href="{{ url("downloads/tcamp2022/2022第30屆教師生命成長營課程表.pdf") }}">課程表</a>-->
+                            <a class="nav-link" href="https://bwfoce.wixsite.com/2023gtcamp">課程表</a>
+                        </li>
+                        <li class="nav-item">
+                            <!--<a class="nav-link" href="{{ url("downloads/tcamp2022/2022教師生命成長營簡章(網頁版)1109F.pdf") }}">報名簡章下載</a>-->
+                            <a class="nav-link" href="https://bwfoce.wixsite.com/2023gtcamp">報名須知</a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url("downloads/tcamp2022/2022第30屆教師生命成長營大專教職員梯-活動簡章_含課表_R5.pdf") }}">課程表</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url("downloads/tcamp2022/2022第30屆教師生命成長營大專教職員梯-活動簡章_含課表_R5.pdf") }}">報名簡章下載</a>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+@endif
+<div class="container w-full">
+    @yield('content')
+    <!-- Site footer -->
+    <footer class='footer'>
+        <p class=text-center>&copy; 財團法人福智文教基金會</p>
+    </footer>
+</div>
+</body></html>
---
+++ resources/views/camps/tvcamp/modifyingSuccessful.blade.php
@@ -0,0 +1,26 @@
+@extends('camps.tvcamp.layout')
+@section('content')
+    <div class='page-header form-group'>
+        <h4>{{ $camp_data->fullName }}</h4>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            修改成功
+        </div>
+        <div class="card-body">
+            <p class="card-text">
+                您成功修改報名 {{ $camp_data->fullName }}（簡稱本營隊）的個人資料。<br>
+                請記下您的<span class="text-danger font-weight-bold">《 報名序號：{{ $applicant->id }} 》</span>作為日後查詢使用。<br>
+                錄取名單將於 5/30、6/15、7/5、7/15、7/30 於<a href="{{ $camp_data->site_url }}">營隊官網</a>公佈，請自行上網查詢。<br>
+                {{-- 錄取結果將在 <u>{{ $camp_data->admission_announcing_date }} ({{ $admission_announcing_date_Weekday }})</u> 起於網上公佈，請自行上網查詢，<br>
+                並於 <u>{{ $camp_data->admission_confirming_end }} ({{ $admission_confirming_end_Weekday }})</u> 前上網回覆確認參加，倘未回覆，視同放棄。 --}}
+            </p>
+            <form action="{{ route("queryview", $applicant->batch_id) }}" method="post" class="d-inline">
+                @csrf
+                <input type="hidden" name="sn" value="{{ $applicant->id }}">
+                <button class="btn btn-primary">檢視報名資料</button>
+            </form>
+            <a href="{{ $camp_data->site_url }}" class="btn btn-primary">回營隊首頁</a>
+        </div>
+    </div>
+@stop
