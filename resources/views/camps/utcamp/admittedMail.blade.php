fe3bf9dc6a9e770919eb11e2807a51e9a1dd3bfb
Minuet <menuett@gmail.com>
2023-11-23 10:08:11 +0800

    update utcamp admission information

--- resources/views/camps/utcamp/admittedMail.blade.php
+++ resources/views/camps/utcamp/admittedMail.blade.php
@@ -36,7 +36,7 @@
         $applicant->xaddr = '台北市南京東路四段165號九樓 福智學堂';
     }
 @endphp
-    <h2 class="center">2023第31屆教師生命成長營</h2>
+    <h2 class="center">2024第32屆教師生命成長營</h2>
     <h2 class="center">大專教職員梯 {{ $applicant->xsession }} 錄取通知單</h2>
 <table width="100%" style="table-layout:fixed; border: 0;">
     <tr>
@@ -46,16 +46,16 @@
         <td>組別：<u>{{ $applicant->group }}</u></td>
     </tr>
 </table>
-    歡迎您參加「2023第31屆教師生命成長營-大專教職員梯 {{ $applicant->xsession }}」，誠摯地期待與您共享這場心靈饗宴，希望您能獲得豐盛的收穫。請詳閱以下相關訊息，祝福您營隊收穫滿滿：<br>
+    歡迎您參加「2024第32屆教師生命成長營-大專教職員場 {{ $applicant->xsession }}」，誠摯地期待與您共享這場心靈饗宴，希望您能獲得豐盛的收穫。請詳閱以下相關訊息，祝福您營隊收穫滿滿：<br>
     <br>
     營隊資訊
     <ol>
-        <li>活動期間：2023/1/31(二)、2/1(三)</li>
-        <li>地點：{{ $applicant->xaddr }}</li>
+        <li>活動期間：2024/1/27(六)～1/30(二)</li>
+        <li>地點：{{ $applicant->batch->location }} {{ $applicant->batch->locationName }}</li>
     </ol>
-    全程參與者，發給研習證明文件或公務員研習證明。<br>
+    全程參與者，發給研習證明文件。<br>
     若有任何問題，歡迎來電(02)7714-6066 分機20318 邱先生。<br>
-    12/12~15將有營隊關懷員與您聯絡，提供營隊相關資訊。<br>
+    12/4(一)~10(日)將有營隊關懷員與您聯絡，提供營隊相關資訊。<br>
 <br>
 <a class="right">財團法人福智文教基金會　謹此</a><br>
 <a class="right">{{ \Carbon\Carbon::now()->year }}  年　{{ \Carbon\Carbon::now()->month }}  月 　 {{ \Carbon\Carbon::now()->day }}  日</a>
