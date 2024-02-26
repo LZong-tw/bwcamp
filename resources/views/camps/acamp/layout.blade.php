cd8d7206a150597396f3f7cd57aaaefe5b1ffe15
Minuet <menuett@gmail.com>
2024-02-26 12:58:59 +0800

    add files for acamp 2024

--- resources/views/camps/acamp/layout.blade.php
+++ resources/views/camps/acamp/layout.blade.php
@@ -63,7 +63,7 @@
                         <a class="nav-link" href="http://www.youngone.org.tw/camp/">課程表</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url("downloads/acamp2023/2023卓越青年生命探索營報名簡章20230408R4.pdf") }}">報名簡章下載</a>
+                        <a class="nav-link" href="{{ url("downloads/acamp2024/2024卓越青年生命探索營報名簡章20240219R1.pdf") }}">報名簡章下載</a>
                     </li>
                 </ul>
             </div>
