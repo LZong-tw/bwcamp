39200f79053d7a30f347ec9ff7460b9f07cccae5
Minuet <menuett@gmail.com>
2024-03-02 08:51:39 +0800

    ceocamp add lrclass

---
+++ database/migrations/2024_03_02_084058_ceocamp_add_lrclass.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ceocamp', function (Blueprint $table) {
+            //
+            $table->string('lrclass')->nullable()->after('contact_time'); //廣論研討班別
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ceocamp', function (Blueprint $table) {
+            //
+            $table->dropColumn('lrclass');
+        });
+    }
+};
