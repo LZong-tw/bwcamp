16387a150c60df24ec5678c158db7501e75f3aab
Minuet <menuett@gmail.com>
2024-03-05 00:08:46 +0800

    update ceovcamp contact info

--- resources/views/camps/ceovcamp/applicantMail.blade.php
+++ resources/views/camps/ceovcamp/applicantMail.blade.php
@@ -5,9 +5,8 @@
     <!-- 錄取名單將於 {{ $applicant->batch->camp->admission_announcing_date }} 後陸續以E-mail通知。<br>-->
     <br>
     洽詢電話：<br>
-    　北區 0917-282-082(秘書組長王雅倫)<br>
+    　北區 0958-903-100(秘書組長葉宜萍)<br>
     　竹區 0933-112-571(李雪貞)<br>
-    　中區 0975-912-525(林映君)<br>
     電子郵件：<br>
     　ceo.camp@blisswisdom.org<br>
     <br>
