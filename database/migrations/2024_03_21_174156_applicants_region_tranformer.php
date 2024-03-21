87182c5ef06b48506b5de7408033dce7803fc600
LZong <lzong.tw@gmail.com>
2024-03-21 17:50:41 +0800

    Transformer.

---
+++ database/migrations/2024_03_21_174156_applicants_region_tranformer.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $regions = \App\Models\Region::all();
+        \DB::transaction(function() use ($regions) {
+            \App\Models\Applicant::query()->whereNull('region_id')->get()->each(function($applicant) use ($regions) {
+                $applicant->region_id = $regions->where('name', $applicant->region)->first()?->id ?? null;
+                $applicant->save();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+};
