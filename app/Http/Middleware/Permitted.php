070f6358431e86d3e0a767d036697e520a09ab6d
LZong <lzong.tw@gmail.com>
2024-04-13 15:44:10 +0800

    .

--- app/Http/Middleware/Permitted.php
+++ app/Http/Middleware/Permitted.php
@@ -18,32 +18,10 @@ class Permitted
         if (!auth()->check()) {
             return abort(404);
         }
-        /**
-         * todo: Request resolver: 檢查目前使用者是否可以取得目標資源
-         */
-        $userPermission = auth()->user()->getPermission('all');
-        foreach($userPermission as $p){
-            if($p->level == 1) {
-                return $next($request);
-            }
-            else if($p->level >= 2 && $p->level <= 6) {
-                if(\Str::contains($p->camp_id, $request->route()->parameter('camp_id'))){
-                    return $next($request);
-                }
-                else if(\Str::contains($p->camp_id, $request->camp_id)){
-                    return $next($request);
-                }
-            }
-        }
         $newRoles = OrgUser::with('camp')->where('user_id', \Auth::user()->id)->get()->pluck('camp.id')->toArray();
         $newRoles = array_filter($newRoles, fn ($value) => !is_null($value));
         if(in_array($request->camp_id, $newRoles)){
             return $next($request);
-        }
-        $newPermissions = \App\Models\User::find(auth()->user()->id)->permissions;
-        if ($newPermissions) {
-            return $next($request);
-            // todo: 實做細節
         }
         if ($request->is('checkin*')) {
             $camp = \App\Models\Camp::find($request->camp_id);
