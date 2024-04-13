5bc6ee9f55d04bd440b2b88bbbec75270b986d16
LZong <lzong.tw@gmail.com>
2024-04-14 02:39:14 +0800

    f5c8da4d0d47427985b6310df3ff7ac6

--- app/Http/Middleware/Permitted.php
+++ app/Http/Middleware/Permitted.php
@@ -23,7 +23,7 @@ class Permitted
         if(in_array($request->camp_id, $newRoles)){
             return $next($request);
         }
-        if ($request->is('checkin*')) {
+        if ($request->is('checkin*') && !$request->is('checkin/selectCamp')) {
             $camp = \App\Models\Camp::find($request->camp_id);
             return response()->view('errors.401',  ['message' => '目前報到營隊為' . $camp->fullName . '，非您可存取'], 401);
         }
