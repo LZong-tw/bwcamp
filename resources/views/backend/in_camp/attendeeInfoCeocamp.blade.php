dcd25829a0a73e3d1a88735b40361413642cbc23
Minuet <menuett@gmail.com>
2024-05-26 14:53:42 +0800

    remove dynamic_url link

--- resources/views/backend/in_camp/attendeeInfoCeocamp.blade.php
+++ resources/views/backend/in_camp/attendeeInfoCeocamp.blade.php
@@ -280,7 +280,7 @@
         <div class="container">
             @foreach($dynamic_stat_urls as $key => $url)
                 <div class="row mt-3">
-                    <a href="{{ $url }}" target="_blank" class="btn btn-primary mb-3">{{ $key }}</a>
+                    <!--a href="{{ $url }}" target="_blank" class="btn btn-primary mb-3">{{ $key }}</a-->
                     <br>
                     <iframe src="{{ $url }}">Your browser isn't compatible</iframe>
                 </div>
