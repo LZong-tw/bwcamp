6f56e0a3991e59d75f2c17a4710c8d891971e4f5
LZong <lzong.tw@gmail.com>
2024-05-31 14:06:12 +0800

    .

--- app/Models/UserApplicantXref.php
+++ app/Models/UserApplicantXref.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 
 class UserApplicantXref extends Model
 {
+    use Cachable;
+
     protected $fillable = [
         'user_id',
         'applicant_id',
