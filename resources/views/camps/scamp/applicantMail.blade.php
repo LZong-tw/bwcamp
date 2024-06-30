7ff283449c7c97fdfd19cb2fe8bfbccdfac9913d
Minuet <menuett@gmail.com>
2024-06-30 17:55:06 +0800

    fix scamp appliantMail typo

--- resources/views/camps/scamp/applicantMail.blade.php
+++ resources/views/camps/scamp/applicantMail.blade.php
@@ -5,10 +5,13 @@
     {{-- $applicant->batch->camp->admission_announcing_date --}} 
     後續會透過E-mail通知您活動訊息。<br>
     <br>
-    若對活相關訊息有任何問題或建議，歡迎洽詢：<br>
+    若對活動相關訊息有任何問題或建議，歡迎洽詢：<br>
     課程資訊聯絡人：0918627969 許小姐<br>
     　　　　　　　　0918031509 李小姐<br>
     　　　　　　　　0939349349 陳小姐<br>
     <br>
     <br>
-<blockquote>全球永續創生推廣協會  敬啟</blockquote>
+<blockquote>
+    主辦：全球永續創生推廣協會<br>
+    協辦：福智文教基金會敬啟　　敬啟
+</blockquote>
