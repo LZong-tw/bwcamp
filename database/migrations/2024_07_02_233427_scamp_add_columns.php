91a90e4d1cb7b5db4e1004c08bc888294d41bd5f
Minuet <menuett@gmail.com>
2024-07-03 00:31:34 +0800

    scamp add column

---
+++ database/migrations/2024_07_02_233427_scamp_add_columns.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('scamp', function (Blueprint $table) {
+            //
+            $table->string('participation_mode')->nullable()->after('is_allow_informed'); //得知管道
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('scamp', function (Blueprint $table) {
+            //
+            $table->dropColumn('participation_mode');
+        });
+    }
+};
