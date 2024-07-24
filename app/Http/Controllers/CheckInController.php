2c72421f6172a9ff7a4a46f7518a8f7a322eedb2
LZong <lzong.tw@gmail.com>
2024-07-24 19:27:45 +0800

    .

--- app/Http/Controllers/CheckInController.php
+++ app/Http/Controllers/CheckInController.php
@@ -344,7 +344,9 @@ class CheckInController extends Controller {
                         ->where(\DB::raw("fee - deposit"), "<=", \DB::raw('0'))
                         ->whereNotNull('group_id')
                         ->where('group_id', '<>', \DB::raw('""'))
-                        ->where([['batch_start', '<=', Carbon::today()], ['batch_end', '>=', Carbon::today()]])->get();
+                        // 這邊可能不需要判斷這麼多
+                        // ->where([['batch_start', '<=', Carbon::today()], ['batch_end', '>=', Carbon::today()]])
+                        ->get();
             $checkedInCount = CheckIn::where('check_in_date', Carbon::today()->format('Y-m-d'))->whereIn('applicant_id', $applicants)->count();
             $applicants = $applicants->count();
             return response()->json([
