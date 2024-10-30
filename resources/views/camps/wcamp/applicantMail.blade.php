9f97e6f5b6f7043c758c0a6f89323eb07a3f28d0
Minuet <menuett@gmail.com>
2024-10-30 11:41:46 +0800

    wcamp wvcamp update and fix bug

--- resources/views/camps/wcamp/applicantMail.blade.php
+++ resources/views/camps/wcamp/applicantMail.blade.php
@@ -6,8 +6,7 @@
     後續會透過E-mail通知您活動訊息。<br>
     <br>
     若對活動相關訊息有任何問題或建議，歡迎洽詢本活動報名承辦人員：<br>
-    張小姐<br>
-    電話:0920-846619<br>
-    email:<a href="mailto:ent.camp@blisswisdom.org">ent.camp@blisswisdom.org</a><br>
+    陳芊羽<br>
+    email:<a href="mailto:54damay@gmail.com">54damay@gmail.com</a><br>
     <br>
-<blockquote>財團法人福智文教基金會  敬啟</blockquote>
+<blockquote>講師培訓營報名服務處  敬啟</blockquote>
--- resources/views/camps/wcamp/successMessages.blade.php
+++ resources/views/camps/wcamp/successMessages.blade.php
@@ -6,7 +6,6 @@
 請確認您收到報名確認信，後續也會透過E-mail通知您活動訊息。<br>
 <br>
 若對活動相關訊息有任何問題或建議，歡迎洽詢本活動報名承辦人員：<br>
-張小姐<br>
-電話:0920-846619<br>
-email:<a href="mailto:ent.camp@blisswisdom.org">ent.camp@blisswisdom.org</a><br>
+陳芊羽<br>
+email:<a href="mailto:54damay@gmail.com">54damay@gmail.com</a><br>
 <br>
