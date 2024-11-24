f670b41347b8cf1bc0a09df1c94e8605f1f6472a
LZong <lzong.tw@gmail.com>
2024-11-24 12:07:19 +0800

    .

--- resources/views/components/table/applicant-list.blade.php
+++ resources/views/components/table/applicant-list.blade.php
@@ -186,7 +186,9 @@
         // remove null item
         result = result.filter(function(item) { return item != null && item != 0; });
         let count = 0;
-        result = Object.values(result[0]);
+        if (result.length == 1) { 
+            result = Object.values(result[0]);
+        }
         result.forEach(function(item) {
             if (!item || !item.id) {
                 console.log(item, count);
