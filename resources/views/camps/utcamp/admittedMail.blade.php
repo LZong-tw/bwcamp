cbaa6a571b84b1fdb285364dabd70b29c05f9129
Minuet <menuett@gmail.com>
2024-11-29 13:25:59 +0800

    utcamp modify mail format

--- resources/views/camps/utcamp/admittedMail.blade.php
+++ resources/views/camps/utcamp/admittedMail.blade.php
@@ -38,6 +38,7 @@
 @endphp
     <h2 class="center">{{ $applicant->batch->camp->fullName }}</h2>
     <h2 class="center">{{ $applicant->batch->name }} 錄取通知單</h2>
+    <br>
 <table width="100%" style="table-layout:fixed; border: 0;">
     <tr>
         <td>報名序號：{{ $applicant->id }}</td>
@@ -46,16 +47,16 @@
         <td>組別：<u>{{ $applicant->group }}</u></td>
     </tr>
 </table>
-    歡迎您參加「{{ $applicant->batch->camp->fullName }} {{ $applicant->batch->name }}」，誠摯地期待與您共享這場心靈饗宴，希望您能獲得豐盛的收穫。請詳閱以下相關訊息，祝福您營隊收穫滿滿。<br>
+    &emsp;&emsp;歡迎您參加「{{ $applicant->batch->camp->fullName }} {{ $applicant->batch->name }}」，誠摯地期待與您共享這場心靈饗宴，希望您能獲得豐盛的收穫。請詳閱以下相關訊息，祝福您營隊收穫滿滿。<br>
     <br>
     營隊資訊
     <ol>
         <li>活動期間：{{ $applicant->batch->batch_start }}({{ $batch_start_Weekday }})～{{ $applicant->batch->batch_end }}({{ $batch_end_Weekday }})</li>
         <li>地點：{{ $applicant->batch->location }} {{ $applicant->batch->locationName }}</li>
     </ol>
-    全程參與者，發給研習證明文件。<br>
-    若有任何問題，歡迎來電(02)7714-6066 分機20318 邱先生。<br>
-    12/10(二)~16(一)將有營隊關懷員與您聯絡，提供營隊相關資訊。<br>
+    &emsp;&emsp;全程參與者，發給研習證明文件。<br><br>
+    &emsp;&emsp;若有任何問題，歡迎來電(02)7714-6066 分機20318 邱先生。<br><br>
+    &emsp;&emsp;12/10(二)~12/16(一)將有營隊關懷員與您聯絡，提供營隊相關資訊。<br><br>
 <br>
 <a class="right">財團法人福智文教基金會　敬上</a><br>
 <a class="right">{{ \Carbon\Carbon::now()->year }}  年　{{ \Carbon\Carbon::now()->month }}  月 　 {{ \Carbon\Carbon::now()->day }}  日</a>
