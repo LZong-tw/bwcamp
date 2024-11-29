01f41b41b045fb75986e8359fc2fdd09028eff3b
Minuet <menuett@gmail.com>
2024-11-29 13:11:39 +0800

    utcamp admission update

--- resources/views/camps/utcamp/admissionResult.blade.php
+++ resources/views/camps/utcamp/admissionResult.blade.php
@@ -17,7 +17,7 @@
                 $skip = true;
             @endphp        
             <div class="alert alert-success" role="alert">
-            您好，感謝您報名 【2024第32屆教師生命成長營 大專教職員梯】，大會預計於：12/1(五)寄發「錄取通知」電子信件(email)，並同步於報名網站提供查詢。
+            您好，感謝您報名 【{{ $applicant->batch->camp->fullName }} {{ $applicant->batch->name }}】，大會預計於：{{ $applicant->batch->camp->admission_announcing_date }}({{ $applicant->batch->camp->admission_announcing_date_Weekday }})寄發「錄取通知」電子信件(email)，並同步於報名網站提供查詢。
             </div>
         @endif
         @if(!$skip)
@@ -25,7 +25,7 @@
                 <div class="col-sm-12">
                     <div class="card">
                         <div class="card-header">
-                            <h5>2024第32屆教師生命成長營 大專教職員場 錄取結果</h5>
+                            <h5>{{ $applicant->batch->camp->fullName }} {{ $applicant->batch->name }} 錄取結果</h5>
                         </div>
                         <div class="card-body">
                             <p class="card-text">
@@ -37,10 +37,10 @@
                                 <td>組別：<u>{{ $applicant->group }}</u></td>
                                 </tr>
                                 </table>
-                                歡迎您參加「2024第32屆教師生命成長營 大專教職員場」，誠摯地期待與您共享這場心靈饗宴，希望您能獲得豐盛的收穫。請詳閱以下相關訊息，祝福您營隊收穫滿滿。
+                                歡迎您參加「{{ $applicant->batch->camp->fullName }} {{ $applicant->batch->name }}」，誠摯地期待與您共享這場心靈饗宴，希望您能獲得豐盛的收穫。請詳閱以下相關訊息，祝福您營隊收穫滿滿。
                                 <h4>營隊資訊</h4>
                                 <div class="ml-4 mb-2">
-                                活動期間：2024/1/27(六)～1/30(二)<br>
+                                活動期間：{{ $applicant->batch->batch_start }}({{ $applicant->batch_start_Weekday }})～{{ $applicant->batch->batch_end }}({{ $applicant->batch_end_Weekday }})<br>
                                 地點：{{ $applicant->batch->location }} {{ $applicant->batch->locationName }}<br>
                                 {{-- 
                                 <h4>請回覆確認參加</h4>
@@ -69,7 +69,7 @@
                                 <div class="mb-2">
                                     全程參與者，發給研習證明文件。<br>
                                     若有任何問題，歡迎來電(02)7714-6066 分機20318 邱先生。<br>
-                                    12/4(一)~10(日)將有營隊關懷員與您聯絡，提供營隊相關資訊。<br>
+                                    12/10(二)~12/16(一)將有營隊關懷員與您聯絡，提供營隊相關資訊。<br>
                                 </div>
                             </p>
                             <input type='button' class='btn btn-warning' value='回上一頁' onclick=self.history.back()>
@@ -104,14 +104,15 @@
                 <div class="col-sm-12">
                     <div class="card">
                         <div class="card-header">
-                            <h5>2024第32屆教師生命成長營 大專教職員場 錄取結果</h5>
+                            <h5>{{ $applicant->batch->camp->fullName }} {{ $applicant->batch->name }} 錄取結果</h5>
                         </div>
                         <div class="card-body">                                
                             <p class="card-text">
-                            敬啟者：<br>
-                            很遺憾通知，因資格問題，您此次未能錄取「2024第32屆教師生命成長營 大專教職員場」。期盼未來仍能在福智文教基金會舉辦之心靈提升、教育活動見到您的身影。<br>
-                            相關資訊請見福智文教基金會官網：<a href="https://bwfoce.org">https://bwfoce.org</a><br>
+                            敬愛的教育夥伴，您好！<br>
+                            　　非常感謝您的報名，但是很遺憾，因資格問題，您此次未獲錄取「{{ $applicant->batch->camp->fullName }} {{ $applicant->batch->name }}」。期盼未來仍能在福智文教基金會舉辦之心靈提升、教育活動見到您的身影。另外，福智文教基金會在各縣市的分支機構，平日都設有適合各年齡層的多元心靈提升課程，誠摯歡迎您的參與！<br>
+                            關注「福智文教基金會」網站：<a href="https://bwfoce.org">https://bwfoce.org</a><br>
                             若有疑問，歡迎來訊：<a href="mailto:bwfaculty@blisswisdom.org">bwfaculty@blisswisdom.org</a><br>
+                            敬祝　教安！<br>
                             </p>
                             <input type='button' class='btn btn-warning' value='回上一頁' onclick=self.history.back()>
                             <a href="{{ $camp_data->site_url }}" class="btn btn-primary">回營隊首頁</a>
