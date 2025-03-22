358d5751d552c6924f546cd3b031ce9c5384ebe4
Minuet <menuett@gmail.com>
2025-03-22 09:31:10 +0800

    utcamp update

--- resources/views/camps/utcamp/successMessages.blade.php
+++ resources/views/camps/utcamp/successMessages.blade.php
@@ -1,4 +1,18 @@
 
-請記下您的<span class="text-danger font-weight-bold">《 報名序號：{{ $applicant->id }} 》</span>作為日後查詢使用。
+請記下您的<span class="text-danger font-weight-bold">《 報名序號：{{ $applicant->id }} 》</span>作為日後查詢使用。<br>
 <br>
+錄取方式​​​：經審核報名資格，將於七日內以email 通知錄取與繳費資訊。請於期限內完成繳費，即正式完成報名！<br>
+​<br>
+取消參加退費原則：<br>
+2025/6/12(含)前可全額退費(需扣除5%手續費)；<br>
+2025/6/13(含)以後恕不退費。<br>
+​<br>
+洽詢電話：(週一 ~ 週五 上午10時 ~ 下午5時)<br>
+        (02)7714-6066 分機 20317 陳小姐<br>
+LINE@：http://bwfoce.org/line<br>
+LINE ID：@bwfoce<br>
+電子郵件：bwfaculty@blisswisdom.org<br> 
+​<br>
+{{--
 請於 <u>{{ \Carbon\Carbon::parse($camp_data->admission_announcing_date)->translatedFormat("Y年m月d日(l)") }}</u> 起，至活動網站 <a href="http://bwfoce.org/2023utcamp">http://bwfoce.org/2023utcamp</a> 查詢是否錄取。
+--}}
