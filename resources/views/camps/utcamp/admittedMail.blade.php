f595a25cc009c0abf1654a970f5f74d87efe20f3
Minuet <menuett@gmail.com>
2025-03-30 17:25:39 +0800

    update admittedMail utcamp

--- resources/views/camps/utcamp/admittedMail.blade.php
+++ resources/views/camps/utcamp/admittedMail.blade.php
@@ -47,10 +47,10 @@
         <td>組別：<u>{{ $applicant->group }}</u></td>
     </tr>
 </table>
-
+<br>
 &emsp;&emsp;歡迎參加「{{ $applicant->batch->camp->fullName }}」，誠摯地期待與您共享這場心靈饗宴，希望您能獲得豐盛的收穫。請詳閱以下相關訊息，祝福您營隊收穫滿滿。<br>
-
-&emsp;&emsp;請於收到錄取通知單七日內（{{ \Carbon\Carbon::now()->addDays(7)->year }}年　{{ \Carbon\Carbon::now()->addDays(7)->month }}月{{ \Carbon\Carbon::now()->addDays(7)->day }}日前）完成轉帳 {{ $applicant->fee }}元整。確認款項後，您的報名才正式生效。匯款帳號如下：<br>
+<br>
+&emsp;&emsp;請於收到錄取通知單七日內（{{ \Carbon\Carbon::now()->addDays(7)->year }}年{{ \Carbon\Carbon::now()->addDays(7)->month }}月{{ \Carbon\Carbon::now()->addDays(7)->day }}日前）完成轉帳 {{ $applicant->fee }}元整。確認款項後，您的報名才正式生效。匯款帳號如下：<br>
 <br>
 &emsp;&emsp;邱孟懿 中國信託 板新分行<br>
 &emsp;&emsp;帳號：417540291289<br>
@@ -65,20 +65,20 @@
     </li>
 
     <li>研習費用<br>
-        含三天兩夜住宿、全程蔬食餐點、新竹高鐵站接駁<br>
+        含三天兩夜住宿、全程蔬食餐點、新竹高鐵站接駁。<br>
         ＊全程參與者，發給研習證明文件。<br>
     </li>
     <li>交通接駁<br>
-    {{ $applicant->batch->batch_start }}前往麻布山林接駁車：將於新竹高鐵站出口4，12:30發車<br>
-    {{ $applicant->batch->batch_end }}前往新竹高鐵站接駁車：由麻布山林山居外P1停車場，17:00發車<br> 
+    {{ $applicant->batch->batch_start }} 前往麻布山林接駁車：將於新竹高鐵站出口4，12:30發車<br>
+    {{ $applicant->batch->batch_end }} 前往新竹高鐵站接駁車：由麻布山林山居外P1停車場，17:00發車<br> 
     </li>
 
-    <li>取消參加退費原則
-    若您因故不克參與，2025/6/12(含)前可全額退費(需扣除5%手續費)；<br>
-    2025/6/13(含)以後恕不退費。<br>
+    <li>取消參加退費原則<br>
+    若您因故不克參與，2025-06-12(含)前可全額退費(需扣除5%手續費)；<br>
+    2025-06-13(含)以後恕不退費。<br>
     </li>
 </ol>
 &emsp;&emsp;若有任何問題，歡迎來電(02)7714-6066 分機20317 陳小姐。或 email: bwfaculty@blisswisdom.org<br>
 <br><br>
 <a class="right">財團法人福智文教基金會　敬上</a><br>
-<a class="right">{{ \Carbon\Carbon::now()->year }}  年　{{ \Carbon\Carbon::now()->month }}  月 　 {{ \Carbon\Carbon::now()->day }}  日</a>
+<a class="right">{{ \Carbon\Carbon::now()->year }} 年 {{ \Carbon\Carbon::now()->month }} 月 {{ \Carbon\Carbon::now()->day }} 日</a>
