4aa042b9fdbef0d3a228eaee41c7c79ec5635dd7
Minuet <menuett@gmail.com>
2025-06-07 14:04:06 +0800

    fix bug

--- resources/views/camps/evcamp/applicantMail.blade.php
+++ resources/views/camps/evcamp/applicantMail.blade.php
@@ -5,13 +5,13 @@
     <!-- 錄取名單將於 {{ $applicant->batch->camp->admission_announcing_date }} 後陸續以E-mail通知。<br>-->
     <br>
     洽詢電話及電子郵件：<br>
-    @if (str_contains($applicant->batch->name, "北區"))
+    @if(str_contains($applicant->batch->name, "北區"))
     紀雅真：0921-059-597 yc890906@gmail.com<br>
     張惠琴：0920-846-619 lilychang846619@gmail.com<br>
     @else
     　中區場：陳蘊蓁 0931-993-726 amanda672672@gmail.com<br>
     　南區場：方嘉麟 0934-151-353 meibao288@gmail.com<br>
-    @end
+    @endif
     <br>
     <br>
 <blockquote>財團法人福智文教基金會  敬啟</blockquote>
