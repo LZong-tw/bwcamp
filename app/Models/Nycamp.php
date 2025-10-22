a0bffd0d430c3e94a2ebd27a943d88742bcadfc8
Minuet <menuett@gmail.com>
2025-10-23 00:00:50 +0800

    modify admittedMail

--- app/Models/Nycamp.php
+++ app/Models/Nycamp.php
@@ -13,8 +13,8 @@ class Nycamp extends Model
 
     protected $fillable = [
         'applicant_id', 'chinese_first_name', 'chinese_last_name', 'english_last_name', 'language',
-        'addr_city', 'addr_state', 'addr_country', 'school', 'department', 'grade', 'unit', 'title',
-        'dietary_needs', 'other_needs', 'accommodation_needs', 'motivation', 'info_source'
+        'addr_city', 'addr_state', 'addr_country', 'is_student', 'school', 'department', 'grade', 'unit', 'title',
+        'dietary_needs', 'other_needs', 'accommodation_needs', 'companion_name', 'companion_as_roommate', 'motivation', 'info_source'
     ];
 
     protected $guarded = [];
