ae16ecbb225f50dacd95f516abf605aa33b87f15
Minuet <menuett@gmail.com>
2025-11-28 11:32:33 +0800

    nycamp add export and discount

---
+++ database/migrations/2025_11_23_213947_batchs_add_columns.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('batchs', function (Blueprint $table) {
+            //
+            $table->text('contact_card')->nullable()->after('num_groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('batchs', function (Blueprint $table) {
+            //
+            $table->dropColumn('contact_card');
+        });
+    }
+};
