f1654dab14ac3aa6f2b9e4e3af47f42c54f30733
minchenho <53116566+minchenho@users.noreply.github.com>
2025-12-07 11:18:45 +0800

    Rename currency methods for consistency

--- app/Models/Traffic.php
+++ app/Models/Traffic.php
@@ -47,12 +47,12 @@ class Traffic extends Model
         return $this->belongsTo(Applicant::class, 'applicant_id', 'id');
     }
 
-    public function getFareCurrency()
+    public function fareCurrency()
     {
         return $this->belongsTo(Currency::class, 'fare_currency_id', 'id');
     }
 
-    public function getDepositCurrency()
+    public function depositCurrency()
     {
         return $this->belongsTo(Currency::class, 'deposit_currency_id', 'id');
     }
