bdf63d961b817936b78dff71d09d058351f273f8
Minuet <menuett@gmail.com>
2025-12-15 01:44:55 +0800

    admissionResult for tcamp utcamp

--- resources/views/camps/tcamp/admissionResult.blade.php
+++ resources/views/camps/tcamp/admissionResult.blade.php
@@ -1,140 +1,110 @@
 @extends('camps.tcamp.layout')
 @section('content')
-    <style>
-        .indent{
-            text-indent: 22px;
-        }
-    </style>
-    <div class='page-header form-group'>
-        <h4>{{ $camp_data->fullName }}</h4>
-    </div>
-    <div class="row">
-        @php
-            $skip = true;
-        @endphp
-        @if(!$skip)
-            @if($applicant->is_admitted && $applicant->workshop_credit_type=="基金會研習數位證明書")
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h2>研習證明下載</h2>
-                    </div>
-                    <div class="card-body">
-                        <a href="https://bwcamp.bwfoce.org/downloads/tcamp2024/{{ $applicant->group }}{{ $applicant->number }}{{ $applicant->applicant_id }}.pdf" target="_blank" rel="noopener noreferrer" class="btn btn-success">下載</a>
-                    </div>
-                    <div class="card-body">
-                        如下載顯示錯誤，請聯絡您的帶組老師，謝謝！
-                    </div>
+<style>
+    u{
+        color: red;
+    }
+    .indent{
+        text-indent: 22px;
+    }
+</style>
+<div class='page-header form-group'>
+    <h4>{{ $camp_data->fullName }}</h4>
+</div>
+<div class="row">
+    <!--研習證明可供下載後，就隱藏錄取查詢-->
+    @if(\Carbon\Carbon::now()->gte($camp_data->certificate_available_date))
+        @if($applicant->is_admitted && $applicant->workshop_credit_type=="基金會研習數位證明書")
+            <div class="card col-sm-12">
+                <div class="card-header">
+                    <h2>研習證明下載</h2>
+                </div>
+                <div class="card-body">
+                    <a href="https://bwcamp.bwfoce.org/downloads/tcamp{{ $applicant->batch->camp->year }}/{{ $applicant->group }}{{ $applicant->number }}{{ $applicant->applicant_id }}.pdf" target="_blank" rel="noopener noreferrer" class="btn btn-success">下載</a>
+                </div>
+                <div class="card-body">
+                    如下載顯示錯誤，請聯絡您的小組關懷員，謝謝！
                 </div>
             </div><br>
-            @endif
-            @if($applicant->is_admitted)
-                <div class="col-sm-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>查詢結果</h4>
-                        </div>
-                        <div class="card-body">
-                            <p class="card-text">
-                                <table width="100%" style="table-layout:fixed; border: 0; text-align: center; margin-bottom: 12px; margin-top: -4px;">
-                                    <tr>
-                                        <td><h6>地點</h6></td>
-                                        <td><h6>時間</h6></td>
-                                        <td><h6>姓名</h6></td>
-                                        <td><h6>錄取編號</h6></td>
-                                        <td><h6>組別</h6></td>
-                                    </tr>
-                                    <tr>
-                                        <td>{{ $applicant->batch->locationName }}</td>
-                                        <td>{{ $applicant->batch->batch_start }}-{{ $applicant->batch->batch_end }}</td>
-                                        <td>{{ $applicant->name }}</td>
-                                        <td><u>{{ $applicant->group }}{{ $applicant->number }}</u></td>
-                                        <td><u>{{ $applicant->group }}</u></td>
-                                        </tr>
-                                </table>
-                                &emsp;&emsp;恭喜您錄取「{{ $applicant->batch->camp->fullName }}」！竭誠歡迎您的到來。<br>
-                                &emsp;&emsp;相關營隊訊息，將於營隊三周前寄發「報到通知單」，請記得到電子信箱收信。<br>
-                                &emsp;&emsp;也歡迎加入[幸福心學堂online]臉書社團，收取營隊訊息和教育新知。<br>
-                                &emsp;&emsp;很期待與您共享這場心靈的饗宴，預祝您歡喜學習，收穫滿滿。<br>
-                                &emsp;&emsp;敬祝　教安<br>
-                                <h5>請加入幸福心學堂online臉書社團</h5>
-                                <div class="ml-4 mb-2">
-                                    <a href="https://www.facebook.com/groups/bwfoce.happiness.new" target="_blank" rel="noopener noreferrer">https://www.facebook.com/groups/bwfoce.happiness.new</a>
-                                </div>
-                                <h5>關注「福智文教基金會」網站：</h5>
-                                <div class="ml-4 mb-2">
-                                    <a href="https://bwfoce.org" target="_blank" rel="noopener noreferrer">https://bwfoce.org</a><br>
-                                </div>
-                                <h5>諮詢窗口</h5>
-                                <div class="ml-4 mb-2">
-                                    報名報到諮詢窗口（周一至周五10:00~17:30）<br>
-                                    王淑靜&emsp;小姐<br>
-                                    電話：07-9769341#413<br>
-                                    Email：shu-chin.wang@blisswisdom.org<br>
-                                </div>
-                                {{-- <h4>營隊資訊</h4>
-                                <div class="ml-4 mb-2">
-                                    活動期間：2022/1/23、24 (日、一)<br>
-                                    本次活動採線上舉辦。 <br>
-                                    連線軟體與帳號：Zoom&nbsp;(請事先下載安裝，當日09:00將開放連線)：<br>
-                                    Zoom 帳號：95556824059 密碼：703112
-                                    </div>
-                                <br><br> --}}
-                                <h5>參加狀態</h5>
-                                <div class="ml-4 mb-2">
-                                    @if(!isset($applicant->is_attend))
-                                        <div class="ml-4 mb-2 text-primary">狀態：未回覆參加。</div>
-                                    @elseif($applicant->is_attend)
-                                        <div class="ml-4 mb-2 text-success">狀態：已確認參加。如欲取消請按取消參加。</div>
-                                    @else
-                                        <div class="ml-4 mb-2 text-danger">狀態：取消參加。如欲恢復參加請按確認參加。</div>
-                                    @endif
-                                    <form class="ml-4 mb-2" action="{{ route('toggleAttend', $batch_id) }}" method="POST">
-                                        @csrf
-                                        <input type="hidden" name="id" value="{{ $applicant->applicant_id ?? $applicant->id }}">
-                                        @if(!isset($applicant->is_attend))
-                                            <input class="btn btn-success" type="submit" value="確認參加">
-                                            <input class="btn btn-danger" type="submit" value="不參加" id="cancel" name="confirmation_no">
-                                        @elseif($applicant->is_attend)
-                                            <input class="btn btn-danger" type="submit" value="取消參加" id="cancel">
-                                        @else
-                                            <input class="btn btn-success" type="submit" value="確認參加">
-                                        @endif
-                                    </form>
-                                    {{--全程參與者，發給研習證明文件。--}}
-                                </div>
-                            </p>
-                            <br><br>
-                            <input type='button' class='btn btn-warning' value='回上一頁' onclick=self.history.back()>
-                            <a href="{{ $camp_data->site_url }}" class="btn btn-primary">回營隊首頁</a>
-                        </div>
-                    </div>
-                </div>
-            @else
-                <div class="col-sm-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>查詢結果</h4>
-                        </div>
-                        <div class="card-body">
-                            <p class="card-text">
-                                敬愛的教育夥伴 {{ $applicant->name }} ，您好！ <br><br>
-                                　　「教師生命成長營」自舉辦以來，每年都得到教育夥伴們的支持和肯定，思及社會上仍有這麼多人共同關心莘莘學子們的學習成長，令人深感振奮！每一位老師的報名都是鼓舞我們的一分力量，激勵基金會全體人員持續不懈，與大家共同攜手為教育盡心盡力。 <br><br>
-                                　　非常感謝您的報名，由於我們的場地容量的侷限性，不克錄取，造成您的不便，敬請見諒包涵！ <br><br>
-                                　　福智文教基金會在全省各縣市的分支機構，平日都設有適合各年齡層的多元心靈提升課程，誠摯歡迎您的參與！<br><br>
-                                　　關注「福智文教基金會」網站：<a href="https://bwfoce.org" target="_blank" rel="noopener noreferrer">https://bwfoce.org</a><br>
-                                　　關注「幸福心學堂online」FB社團：<a href="https://www.facebook.com/groups/bwfoce.happiness.new/" target="_blank" rel="noopener noreferrer">https://www.facebook.com/groups/bwfoce.happiness.new</a><br>
-                                <br>
-                                敬祝　教安！
-                            </p>
-                            <p class="right">財團法人福智文教基金會　謹此</p>
-                            <p class="right">{{ \Carbon\Carbon::now()->year }}  年 {{ \Carbon\Carbon::now()->month }} 月 {{ \Carbon\Carbon::now()->day }} 日</p>
-                            <input type='button' class='btn btn-warning' value='回上一頁' onclick=self.history.back()>
-                            <a href="{{ $camp_data->site_url }}" class="btn btn-primary">回營隊首頁</a>
-                        </div>
-                    </div>
-                </div>
-            @endif
         @endif
-    </div>
+    @else
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5>{{ $applicant->batch->camp->fullName }} 錄取查詢結果</h5>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                    @if($applicant->is_admitted)
+
+                        {{ $applicant->name }} 您好！<br>
+                        　　恭喜您錄取「{{ $applicant->batch->camp->fullName }}」，竭誠歡迎您的到來。<br>
+                        我們將於營隊三周前寄發「報到通知單」，提供營隊相關訊息，請記得到電子信箱收信。<br>
+                        營隊之前，歡迎您加入「<a href="https://www.facebook.com/groups/bwfoce.happiness.new" target="_blank" rel="noopener noreferrer">幸福心學堂online</a>」臉書社團，收取營隊訊息和教育新知。<br>
+                        期待與您共享這場心靈的饗宴，預祝您歡喜學習，收穫滿滿。<br>
+                        　　敬祝<br>
+                        教安<br>
+
+                        <h5>報名/錄取/營隊資訊</h5>
+                        <div class="ml-4 mb-2">
+                        您的姓名：{{ $applicant->name }}<br>
+                        報名序號：{{ $applicant->applicant_id }}<br>
+                        錄取編號：<u>{{ $applicant->group }}{{ $applicant->number }}</u><br>
+                        組別：<u>{{ $applicant->group }}</u><br>
+                        --<br>
+                        營隊日期：{{ $applicant->batch->batch_start }}({{ $applicant->batch_start_Weekday }})～{{ $applicant->batch->batch_end }}({{ $applicant->batch_end_Weekday }})<br>
+                        營隊地點：{{ $applicant->batch->location }} {{ $applicant->batch->locationName }}<br>
+                        --<br>
+                        <b>報名報到諮詢窗口</b>（周一至周五10:00~17:30）<br>
+                        　　王淑靜 小姐<br>
+                        　　電話：07-9769341#413<br>
+                        　　Email：shu-chin.wang@blisswisdom.org<br>
+                        </div>
+                        <br>
+                        <h5>您的參加狀態</h5>
+                        <div class="ml-4 mb-2">
+                            @if(!isset($applicant->is_attend))
+                                <div class="text-primary">狀態：未回覆參加。</div>
+                            @elseif($applicant->is_attend)
+                                <div class="text-success">狀態：已確認參加。如欲取消請按取消參加。</div>
+                            @else
+                                <div class="text-danger">狀態：取消參加。如欲恢復參加請按確認參加。</div>
+                            @endif
+                            <form class="" action="{{ route('toggleAttend', $batch_id) }}" method="POST">
+                                @csrf
+                                <input type="hidden" name="id" value="{{ $applicant->applicant_id ?? $applicant->id }}">
+                                @if(!isset($applicant->is_attend))
+                                    <input class="btn btn-success" type="submit" value="確認參加">
+                                    <input class="btn btn-danger" type="submit" value="不參加" id="cancel" name="confirmation_no">
+                                @elseif($applicant->is_attend)
+                                    <input class="btn btn-danger" type="submit" value="取消參加" id="cancel">
+                                @else
+                                    <input class="btn btn-success" type="submit" value="確認參加">
+                                @endif
+                            </form>
+                            {{--全程參與者，發給研習證明文件。--}}
+                        </div>
+                    @else
+                        敬愛的教育夥伴 {{ $applicant->name }} ，您好！ <br>
+                        　　「教師生命成長營」自舉辦以來，每年都得到教育夥伴們的支持和肯定，思及社會上仍有這麼多人共同關心莘莘學子們的學習成長，令人深感振奮！每一位老師的報名都是鼓舞我們的一分力量，激勵基金會全體人員持續不懈，與大家共同攜手為教育盡心盡力。 <br>
+                        　　非常感謝您的報名，由於我們的場地容量的侷限性，不克錄取，造成您的不便，敬請見諒包涵。 福智文教基金會在全省各縣市的分支機構，平日都設有適合各年齡層的多元心靈提升課程，誠摯歡迎您的參與！<br>
+                        　　敬祝<br>
+                        教安<br>
+                    @endif
+                    <br>
+                    <p class="right">財團法人福智文教基金會　謹此<br>
+                    {{ \Carbon\Carbon::now()->year }}  年 {{ \Carbon\Carbon::now()->month }} 月 {{ \Carbon\Carbon::now()->day }} 日</p>
+                    <b>「福智文教基金會」網站：</b>
+                    <a href="https://bwfoce.org" target="_blank" rel="noopener noreferrer">https://bwfoce.org</a><br>
+                    <b>「幸福心學堂online」臉書社團：</b>
+                    <a href="https://www.facebook.com/groups/bwfoce.happiness.new" target="_blank" rel="noopener noreferrer">https://www.facebook.com/groups/bwfoce.happiness.new</a><br>
+                    <br>
+                    <input type='button' class='btn btn-warning' value='回上一頁' onclick=self.history.back()>
+                    <a href="{{ $camp_data->site_url }}" class="btn btn-primary">回營隊首頁</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    @endif
+</div>
 @stop
--- resources/views/camps/utcamp/admissionResult.blade.php
+++ resources/views/camps/utcamp/admissionResult.blade.php
@@ -1,125 +1,90 @@
 @extends('camps.utcamp.layout')
 @section('content')
-    <style>
-        .indent{
-            text-indent: 22px;
-        }
-    </style>
-    <div class='page-header form-group'>
-        <h4>{{ $camp_data->fullName }}</h4>
-    </div>
-    <div class="row">
-        @php
-            $skip = false;   
-        @endphp
-        @if($camp_data->variant == "utcamp" && now()->lt("2021-11-30"))
-            @php
-                $skip = true;
-            @endphp        
-            <div class="alert alert-success" role="alert">
-            您好，感謝您報名 【{{ $applicant->batch->camp->fullName }} {{ $applicant->batch->name }}】，大會預計於：{{ $applicant->batch->camp->admission_announcing_date }}({{ $applicant->batch->camp->admission_announcing_date_Weekday }})寄發「錄取通知」電子信件(email)，並同步於報名網站提供查詢。
+<style>
+    u{
+        color: red;
+    }
+    .indent{
+        text-indent: 22px;
+    }
+</style>
+<div class='page-header form-group'>
+    <h4>{{ $camp_data->fullName }}</h4>
+</div>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card">
+            <div class="card-header">
+                <h5>{{ $applicant->batch->camp->fullName }} 錄取查詢結果</h5>
             </div>
-        @endif
-        @if(!$skip)
-            @if($applicant->is_admitted)
-                <div class="col-sm-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h5>{{ $applicant->batch->camp->fullName }} {{ $applicant->batch->name }} 錄取結果</h5>
-                        </div>
-                        <div class="card-body">
-                            <p class="card-text">
-                                <table width="100%" style="table-layout:fixed; border: 0;">
-                                <tr>
-                                <td>報名序號：{{ $applicant->applicant_id }}</td>
-                                <td>姓名：{{ $applicant->name }}</td>
-                                <td>錄取編號：<u>{{ $applicant->group }}{{ $applicant->number }}</u></td>
-                                <td>組別：<u>{{ $applicant->group }}</u></td>
-                                </tr>
-                                </table>
-                                歡迎您參加「{{ $applicant->batch->camp->fullName }} {{ $applicant->batch->name }}」，誠摯地期待與您共享這場心靈饗宴，希望您能獲得豐盛的收穫。請詳閱以下相關訊息，祝福您營隊收穫滿滿。
-                                <h4>營隊資訊</h4>
-                                <div class="ml-4 mb-2">
-                                活動期間：{{ $applicant->batch->batch_start }}({{ $applicant->batch_start_Weekday }})～{{ $applicant->batch->batch_end }}({{ $applicant->batch_end_Weekday }})<br>
-                                地點：{{ $applicant->batch->location }} {{ $applicant->batch->locationName }}<br>
-                                {{-- 
-                                <h4>請回覆確認參加</h4>
-                                <div class="ml-4 mb-2">
-                                    @if(!isset($applicant->is_attend))
-                                    <div class="ml-4 mb-2 text-primary">狀態：未回覆參加。</div>
-                                    @elseif($applicant->is_attend)
-                                    <div class="ml-4 mb-2 text-success">狀態：已確認參加。</div>
-                                    @else
-                                    <div class="ml-4 mb-2 text-danger">狀態：不參加。</div>
-                                    @endif
-                                    <form class="ml-4 mb-2" action="{{ route('toggleAttend', $batch_id) }}" method="POST">
-                                        @csrf
-                                        <input type="hidden" name="id" value="{{ $applicant->applicant_id ?? $applicant->id }}">
-                                        @if(!isset($applicant->is_attend))
-                                        <input class="btn btn-success" type="submit" value="確認參加">
-                                        <input class="btn btn-danger" type="submit" value="不參加" id="cancel" name="confirmation_no">
-                                        @elseif($applicant->is_attend)
-                                        <input class="btn btn-danger" type="submit" value="取消參加" id="cancel">
-                                        @else
-                                        <input class="btn btn-success" type="submit" value="確認參加">
-                                        @endif
-                                    </form> 
-                                    --}}
-                                </div>
-                                <div class="mb-2">
-                                    全程參與者，發給研習證明文件。<br>
-                                    若有任何問題，歡迎來電(02)7714-6066 分機20318 邱先生。<br>
-                                    12/10(二)~12/16(一)將有營隊關懷員與您聯絡，提供營隊相關資訊。<br>
-                                </div>
-                            </p>
-                            <input type='button' class='btn btn-warning' value='回上一頁' onclick=self.history.back()>
-                            <a href="{{ $camp_data->site_url }}" class="btn btn-primary">回營隊首頁</a>
-                            </div>
-                        </div>
+            <div class="card-body">
+                <p class="card-text">
+                @if($applicant->is_admitted)
+                    {{ $applicant->name }} 您好！<br>
+                    　　恭喜您錄取「{{ $applicant->batch->camp->fullName }}」，竭誠歡迎您的到來。<br>
+                    我們將於營隊三周前寄發「報到通知單」，提供營隊相關訊息，請記得到電子信箱收信。<br>
+                    期待與您共享這場心靈的饗宴，預祝您歡喜學習，收穫滿滿。<br>
+                    　　敬祝<br>
+                    教安<br>
+                    <h5>報名/錄取/營隊資訊</h5>
+                    <div class="ml-4 mb-2">
+                    您的姓名：{{ $applicant->name }}<br>
+                    報名序號：{{ $applicant->applicant_id }}<br>
+                    錄取編號：<u>{{ $applicant->group }}{{ $applicant->number }}</u><br>
+                    組別：<u>{{ $applicant->group }}</u><br>
+                    --<br>
+                    營隊日期：{{ $applicant->batch->batch_start }}({{ $applicant->batch_start_Weekday }})～{{ $applicant->batch->batch_end }}({{ $applicant->batch_end_Weekday }})<br>
+                    營隊地點：{{ $applicant->batch->location }} {{ $applicant->batch->locationName }}<br>
+                    --<br>
+                    <b>報名報到諮詢窗口</b>（周一至周五10:00~17:30）<br>
+                    　　王淑靜 小姐<br>
+                    　　電話：07-9769341#413<br>
+                    　　Email：shu-chin.wang@blisswisdom.org<br>
                     </div>
-                {{-- 
-                    <div class="col-sm-2">
-                    @if($applicant->showCheckInInfo)
-                        <div class="card">
-                            <div class="card-header">
-                                報到資訊
-                            </div>
-                            <div class="card-body">
-                                <form action="{{ route          ("downloadCheckInNotification", $applicant->batch_id) }}" method="post" name="downloadCheckInNotification">
-                                @csrf
-                                <input type="hidden" name="applicant_id" value="{{ $applicant->applicant_id }}">
-                                <button class="btn btn-primary" onclick="this.innerText = '正在產生報到通知單'; this.disabled = true; document.downloadCheckInNotification.submit();">報到通知單</button>
-                                </form>
-                                <form action="{{ route("downloadCheckInQRcode", $applicant->batch_id) }}" method="post" name="downloadCheckInQRcode">
-                                @csrf
-                                <input type="hidden" name="applicant_id" value="{{ $applicant->applicant_id }}">
-                                <button class="btn btn-success" onclick="this.innerText = '正在產生 QR code 報到單'; this.disabled = true; document.downloadCheckInQRcode.submit();">QR Code 報到單</button>
-                                </form>
-                            </div>
-                        </div>
-                    @endif
-                    </div> 
-                --}}
-            @else
-                <div class="col-sm-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h5>{{ $applicant->batch->camp->fullName }} {{ $applicant->batch->name }} 錄取結果</h5>
-                        </div>
-                        <div class="card-body">                                
-                            <p class="card-text">
-                            敬愛的教育夥伴，您好！<br>
-                            　　非常感謝您的報名，但是很遺憾，因資格問題，您此次未獲錄取「{{ $applicant->batch->camp->fullName }} {{ $applicant->batch->name }}」。期盼未來仍能在福智文教基金會舉辦之心靈提升、教育活動見到您的身影。另外，福智文教基金會在各縣市的分支機構，平日都設有適合各年齡層的多元心靈提升課程，誠摯歡迎您的參與！<br>
-                            關注「福智文教基金會」網站：<a href="https://bwfoce.org">https://bwfoce.org</a><br>
-                            若有疑問，歡迎來訊：<a href="mailto:bwfaculty@blisswisdom.org">bwfaculty@blisswisdom.org</a><br>
-                            敬祝　教安！<br>
-                            </p>
-                            <input type='button' class='btn btn-warning' value='回上一頁' onclick=self.history.back()>
-                            <a href="{{ $camp_data->site_url }}" class="btn btn-primary">回營隊首頁</a>
-                        </div>
+                    <br>
+                    <h5>您的參加狀態</h5>
+                    <div class="ml-4 mb-2">
+                        @if(!isset($applicant->is_attend))
+                            <div class="text-primary">狀態：未回覆參加。</div>
+                        @elseif($applicant->is_attend)
+                            <div class="text-success">狀態：已確認參加。如欲取消請按取消參加。</div>
+                        @else
+                            <div class="text-danger">狀態：取消參加。如欲恢復參加請按確認參加。</div>
+                        @endif
+                        <form class="" action="{{ route('toggleAttend', $batch_id) }}" method="POST">
+                            @csrf
+                            <input type="hidden" name="id" value="{{ $applicant->applicant_id ?? $applicant->id }}">
+                            @if(!isset($applicant->is_attend))
+                                <input class="btn btn-success" type="submit" value="確認參加">
+                                <input class="btn btn-danger" type="submit" value="不參加" id="cancel" name="confirmation_no">
+                            @elseif($applicant->is_attend)
+                                <input class="btn btn-danger" type="submit" value="取消參加" id="cancel">
+                            @else
+                                <input class="btn btn-success" type="submit" value="確認參加">
+                            @endif
+                        </form>
                     </div>
-                </div>
-            @endif
-        @endif
+                @else
+                    敬愛的教育夥伴 {{ $applicant->name }} ，您好！ <br>
+                    　　非常感謝您的報名，但是很遺憾，因資格問題，您此次未獲錄取「{{ $applicant->batch->camp->fullName }} 
+                    期盼未來仍能在福智文教基金會舉辦之心靈提升、教育活動見到您的身影。<br>
+                    　　另外，福智文教基金會在各縣市的分支機構，平日都設有適合各年齡層的多元心靈提升課程，誠摯歡迎您的參與！<br>
+                    　　敬祝<br>
+                    教安<br>
+                @endif
+                <br>
+                <p class="right">財團法人福智文教基金會　謹此<br>
+                {{ \Carbon\Carbon::now()->year }}  年 {{ \Carbon\Carbon::now()->month }} 月 {{ \Carbon\Carbon::now()->day }} 日</p>
+                <b>「福智文教基金會」網站：</b>
+                <a href="https://bwfoce.org" target="_blank" rel="noopener noreferrer">https://bwfoce.org</a><br>
+                <b>「幸福心學堂online」臉書社團：</b>
+                <a href="https://www.facebook.com/groups/bwfoce.happiness.new" target="_blank" rel="noopener noreferrer">https://www.facebook.com/groups/bwfoce.happiness.new</a><br>
+                <br>
+                <input type='button' class='btn btn-warning' value='回上一頁' onclick=self.history.back()>
+                <a href="{{ $camp_data->site_url }}" class="btn btn-primary">回營隊首頁</a>
+                </p>
+            </div>
+        </div>
     </div>
+</div>
 @stop
