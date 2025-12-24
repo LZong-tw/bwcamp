885df1a5aa6c323cd366e5accf8ebfacceb83b88
Minuet <menuett@gmail.com>
2025-12-24 21:39:46 +0800

    add mcamp and mvcamp also fix some files from other camps

---
+++ resources/views/camps/mvcamp/applicantMail.blade.php
@@ -0,0 +1,10 @@
+{{ $applicant->name }} 您好：<br>
+<br>
+　　恭喜您已完成{{ $applicant->batch->camp->fullName }}網路報名程序。請記下您的《 報名序號：{{ $applicant->id }} 》作為日後查詢使用。<br>
+<br>
+　　如您需修改報名表內容，可憑報名序號至 <a href="https://bwcamp.bwfoce.org/camp/{{ $applicant->batch->id }}/query"> https://bwcamp.bwfoce.org/camp/{{ $applicant->batch->id }}/query</a> 頁面修改<br>
+    <br>
+    洽詢電話及電子郵件：<br>
+    　　陳芊羽 54damay@gmail.com<br>
+    <br>
+<blockquote>講師培訓營報名服務處  敬啟</blockquote>
