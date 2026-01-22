cba7879868fce4b5277b6f28d0374467cc19b7a4
Minuet <menuett@gmail.com>
2026-01-22 18:46:36 +0800

    change outdatedMessage

--- resources/views/camps/mvcamp/outdated.blade.php
+++ resources/views/camps/mvcamp/outdated.blade.php
@@ -4,7 +4,7 @@
         <h4>{{ $camp_data->fullName }}線上報名表</h4>
     </div>
     <div class='alert alert-danger' role='alert'>
-        <!--報名期限已過，敬請見諒。-->
-        {{ $outdatedMessage }}
+        因報名踴躍，已超過人數限制，敬請見諒
+        <!-- {{ $outdatedMessage }}-->
     </div>
 @stop
