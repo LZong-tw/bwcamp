c0c1e9ab15b1a6fee134f7509ab7e57a1dcd47c8
minchenho <53116566+minchenho@users.noreply.github.com>
2026-02-03 22:47:29 +0800

    Update Mvcamp.php

--- app/Models/Mvcamp.php
+++ app/Models/Mvcamp.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mvcamp extends Model
 {
+    //利用self_intro快速篩出義工窗口
+    //第一年舉辦；暫時的solution
     public const DESCRIPTION_UNIFIED_CONTACT = '第5組義工窗口';
 
     protected $table = 'mvcamp';
