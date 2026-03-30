7fea9f570795322d3348261dde77bc10da81a962
Minuet <menuett@gmail.com>
2026-03-30 13:14:00 +0800

    clean up blade.php

--- resources/views/camps/ecamp/modifyingSuccessful.blade.php
+++ resources/views/camps/ecamp/modifyingSuccessful.blade.php
@@ -1,24 +1,24 @@
-@extends('camps.ecamp.layout')
+@extends('camps.'. $camp_info->table .'.layout')
 @section('content')
     <div class='page-header form-group'>
-        <h4>{{ $camp_data->fullName }}</h4>
+        <h4>{{ $camp_info->fullName }}</h4>
     </div>
     <div class="card">
         <div class="card-header">
             @if(isset($isModify))
-            	修改成功
+            	<h5>修改成功</h5>
             @else
-            	報名成功
+            	<h5>報名成功</h5>
             @endif
         </div>
         <div class="card-body">
             <p class="card-text">
                 @if(isset($isModify))
-                    您成功修改報名 {{ $camp_data->fullName }}的個人資料。<br>
+                    您成功修改「{{ $camp_info->fullName }}」的報名資料。<br>
                 @else
-                    恭喜您已完成「{{ $camp_data->fullName }}」網路報名程序。
+                    恭喜您已完成「{{ $camp_info->fullName }}」網路報名程序。<br>
                 @endif
-                @include('camps.ecamp.successMessages')
+                @include('camps.'. $camp_info->table .'.successMessages')
             </p>
             <form action="{{ route("queryview", $applicant->batch_id) }}" method="post" class="d-inline">
                 @csrf
@@ -26,7 +26,7 @@
                 <input type="hidden" name="name" value="{{ $applicant->name }}">
                 <button class="btn btn-primary">檢視報名資料</button>
             </form>
-            <a href="{{ $camp_data->site_url }}" class="btn btn-info">回營隊首頁</a>
+            <a href="{{ $camp_info->site_url }}" class="btn btn-warning">回營隊首頁</a>
         </div>
     </div>
 @stop
